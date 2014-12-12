c57a84038d3bdb15257ff2e77a97dea0a24dcf5f
inanhkn <hakan@ubuntu.ubuntu-domain>
2014-12-12 11:54:16 -0800

    changed the script to a function

--- view_behavior_with_licks.m
+++ view_behavior_with_licks.m
@@ -1,20 +1,23 @@
-% ANALYZE BEHAVIOR VIDEO
-clear all; close all;
-
-% Read maze data
-%------------------------------------------------------------
-source = 'hand';
-lick_source = 'hand-lick.txt';
-fps = 20;
+function view_behavior_with_licks(video_source,lick_source)
+%function view_behavior_with_licks(video_source,lick_source)
+%
+%View the behavior video with a superimposed rectangular box in the upper 
+%left corner that indicates whether there is a lick at a given moment 
+%
+%   video_source: .mp4 file to be read as the behavior video source
+%
+%   lick_source: .txt file that contains a binary array with length equal 
+%   to thenumber of frames in the video_source (syncing of the lick_source
+%   and video_source is assumed)
+%
+%Hakan Inan(Dec 14)
+%
 %Read the lick file
 fileID = fopen(lick_source,'r');
 lick_data = fscanf(fileID,'%d');
 num_frames = length(lick_data);
-
-
-% Read video file (kind of slow)
-%------------------------------------------------------------
-behavior_vid = VideoReader(strcat(source,'.mp4'));
+% Read video file 
+behavior_vid = VideoReader(video_source);
 figure,
 %the shape to display to indicate lick/no lick
 for k = 1:num_frames
