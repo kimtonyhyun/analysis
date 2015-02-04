3297a8f8fd564ca57faf622a44848a492ca54980
inanhkn <inanh@stanford.edu>
2015-02-04 15:45:00 -0800

    make summary of the IC traces, behavior movie, and the trial information

---
+++ image_op/get_spatial_trace_from_behavior.m
@@ -0,0 +1,62 @@
+function spatial_trace = get_spatial_trace_from_behavior(sample_image,video)   
+%function spatial_trace = get_spatial_trace_from_behavior(sample_image,video)
+%
+%Extract X and Y coordinates of the mouse from the behavior video
+%
+%   sample_image : A snapshot the behavior setup for use in the maze region
+%   extraction step.
+%
+%   video : [Height x Width x num_frames] array
+%
+%   spatial_trace : [num_frames x 2] matrix where the 1st column is the X-coordinate and
+%   the 2nd column is the Y-coordinate of the mouse. 
+
+    
+    num_frames = size(video,3);
+    centroids = zeros(num_frames,2); 
+    
+    %anything above thresh_whiteness is considered white
+    thresh_whiteness = 100;
+    
+    %anything below thresh_blackness is considered black
+    thresh_blackness = 20;
+    c_old = [0 0];
+    
+    %extract roughly the maze from the sample image (based on whiteness)
+    if size(sample_image,3)>1
+        maze_image = rgb2graysample_image; %convert to grayscale
+    else
+        maze_image = sample_image;
+    end
+    maze_image = medfilt2(maze_image,[20,20]); %blur
+    maze_image(maze_image<thresh_whiteness) = 0; %apply threshold
+    maze_image = logical(maze_image); %binarize
+    for frame_idx = 1:num_frames
+        % Update original image CData
+        frame = video(:,:,frame_idx);  
+        %make off-maze region bright (bright is non-interesting)
+        frame(maze_image == 0) = 255;
+        % binarize image by thresholding (black is interesting - mouse is black)
+        binary_image = (frame < thresh_blackness) ;       
+        %get rid of smaller dots that are not the mouse
+        binary2 = bwareaopen(binary_image,100,8);      
+        % get centroid, area, and length of blobs
+        s = regionprops(logical(binary2(:,:,1)), 'area','centroid','minoraxislength');
+        area_vector = [s.Area];
+        length_vector = [s.MinorAxisLength];
+        centroid_vector = [s.Centroid];
+        %we need to reshape this to a num_centroids by 2 matrix:
+        centroid_vector = reshape(centroid_vector,2,length(centroid_vector)/2)';
+              
+        if isempty(area_vector) %sometimes blob disappears, go to previous centroid
+            centroids(frame_idx,:) = c_old;
+        else %when the blob is there (should be most times)
+            [~, idx] = max(length_vector); %assume mouse is the fattest blob
+            c_new = centroid_vector(idx(1),:);
+%             c_new = s(idx(1)).Centroid(1:2);
+            centroids(frame_idx,:) = c_new;            
+            % Update centroids on original and binary2 using new data
+            c_old = c_new;
+        end    
+    end
+    spatial_trace = centroids;
---
+++ utils/make_summary.m
@@ -0,0 +1,162 @@
+function make_summary(sources,ic_dir,bg_image)
+%function make_summary(sources)
+%
+%Combine everything that belongs to a multiday data in a single cell array
+%
+%   sources : a struct that has as its fields the filenames for the maze 
+%   data, behavior videos, miniscope videos, PCA data, and IC directory. 
+%   (refer to a 'data_sources.m' file for the specifics of this struct. )
+%
+%   The function creates a .mat file with the name 'summary+(current 
+%   directory name)+.mat'. It contains a cell of two cell arrays:
+%     'summary_cell'(of size (#of ICs) x 3) : Columns are for neuron 
+%            classification output, ica filter, and center x-y coordinates
+%            for each  neuron, respectively.
+%     'summary_trial' (of size (#of trials) x 5 ) :  Columns are for start arm,
+%            end arm, correctness, ica traces, and spatial trace (extracted 
+%            from the behavior video) for each trial, respectively.
+%
+%Hakan Inan (Jan 15)
+%
+
+ica_source   = get_most_recent_file(ic_dir, 'ica_*.mat');
+class_source = get_most_recent_file(ic_dir, 'class_*.txt');
+load(ica_source);
+class = load_classification(class_source);
+num_ics = ica_info.num_ICs;
+%%%
+%handle the struct 'sources' and extract the number of days from it
+%%%
+str_sources = fieldnames(sources);
+num_fields = length(str_sources);
+%find number of occurrences of 'maze*' and 'behavior*'
+num_occ_maze = 0;
+num_occ_behavior = 0;
+for k = 1:num_fields
+    if ~isempty(strfind(str_sources{k},'maze'))
+        num_occ_maze = num_occ_maze+1;
+    end
+    if ~isempty(strfind(str_sources{k},'behavior'))
+        num_occ_behavior = num_occ_behavior+1;
+    end
+end
+if num_occ_behavior ~= num_occ_maze
+    error('Number of maze files needs to match the number of behavior videos');
+else
+    num_days = num_occ_maze;
+end
+
+num_frames = size(ica_traces,1);
+size_behavior_vid = [480,640,num_frames];
+%%%
+%Get trial info from maze output
+%%%
+%Combine all the days together and treat the whole thing as a single day
+trial_start_arms = [];
+trial_finish_arms = [];
+trial_correctness = [];
+compressed_indices = [];
+compressed_behavior_vid = zeros(size_behavior_vid,'uint8');
+%keep track of the last frame index of the last session to add to the next
+last_frame = 0;
+%The most time consuming task in the following for loop is compressing the
+%behavior video. Notifying the user when it starts and finishes would be
+%nice
+disp('compressing the behavior video..');
+for idx_day = 1:num_days,
+    if num_days == 1,
+        idx_day = [];
+    end
+    relevant_maze = eval(['sources.maze',num2str(idx_day)]);
+    relevant_behavior_vid_str = eval(['sources.behavior',num2str(idx_day)]);
+    relevant_behavior_vid = VideoReader(relevant_behavior_vid_str); 
+    trial_start_arms  = [trial_start_arms;get_trial_start_arm(relevant_maze)];
+    trial_finish_arms = [trial_finish_arms;get_trial_finish_arm(relevant_maze)];
+    trial_correctness = [trial_correctness;get_trial_correctness(relevant_maze)];
+    %compute the trial frames:
+    trial_frame_indices = get_trial_frame_indices(relevant_maze);
+    compressed_indices = [compressed_indices; last_frame+ ...
+        compress_frame_indices(trial_frame_indices,sources.trim)];       
+    %read the whole behavior movie:
+    whole_behavior_vid = read(relevant_behavior_vid);
+    %calculate frames to include in the compressed behavior movie
+    num_trials_total = size(trial_frame_indices,1);
+    frames = [];
+    trim_before = sources.trim(1);trim_after = sources.trim(2);
+    for k = 1:num_trials_total
+        frames = [frames, (trial_frame_indices(k,1)+trim_before):...
+            (trial_frame_indices(k,4)-trim_after)];
+    end
+    compressed_behavior_vid(:,:,last_frame+1:last_frame+length(frames)) = ...
+    squeeze(whole_behavior_vid(:,:,1,frames)); %keep one of the 3 channels
+    clear whole_behavior_vid;   
+    %update last_frame
+    last_frame = last_frame + compressed_indices(end,end);
+end
+disp('finished compressing the behavior video');
+num_trials_total = size(compressed_indices,1);
+%%%
+%construct 'summary_cell', (#of ICs) x 3 cell array
+%%%
+%for below 1:cell type 2:cell ica filters 3:cell coordinate
+summary_cell = cell(num_ics,3);
+sizeImg = size(ica_filters(:,:,1));
+%matrices pix_x,pix_y come into play when calculating the center of an IC:
+pix_x = repmat(1:sizeImg(2),sizeImg(1),1);
+pix_y = repmat(1:sizeImg(1),sizeImg(2),1)';
+Aa = zeros(num_ics,2);
+for ic_idx = 1:num_ics
+    summary_cell{ic_idx,1} = class{ic_idx};
+    summary_cell{ic_idx,2} = ica_filters(:,:,ic_idx);
+    if (strcmp(class{ic_idx}, 'not a cell'))
+        summary_cell{ic_idx,3}=0;summary_cell{ic_idx,3}=0;
+        continue;
+    end   
+    ic_img = ica_filters(:,:,ic_idx);
+    ic_img = threshold_ic_filter(ic_img, 0.3);
+    %calculate the center of mass of the IC   
+    ic_img = ic_img / sum(sum(ic_img)); %make the image sum to 1
+    center_x = round(sum(sum(pix_x.*ic_img)));
+    center_y = round(sum(sum(pix_y.*ic_img)));
+    summary_cell{ic_idx,3} =  [center_x,center_y];
+end
+%%%
+%construct 'summary_trial', (#of trials) x 5 cell array
+%%%
+%background image to use in the extraction of mouse coordinates
+bg_image = squeeze(bg_image(:,:,1)); % use 1st channel only
+%for below, 1:start arm 2:end arm 3:correctness 4:traces 5:spatial trace 
+summary_trial = cell(num_trials_total,5);
+for trial = 1:num_trials_total
+    frames = compressed_indices(trial,[1,4]);
+    summary_trial{trial,1} = trial_start_arms{trial};
+    summary_trial{trial,2} = trial_finish_arms{trial};
+    summary_trial{trial,3} = trial_correctness(trial);
+    summary_trial{trial,4} = ica_traces(frames(1):frames(2),:)';
+    video = compressed_behavior_vid(:,:,frames(1):frames(2));
+    disp(sprintf('getting xy trace for the trial %d',trial));
+    summary_trial{trial,5} = get_spatial_trace_from_behavior(bg_image,video);    
+end
+
+
+summary = cell(1,2);
+summary{1} = summary_cell;
+summary{2} = summary_trial;
+%get current folder name:
+str = pwd;
+for k = length(str):-1:1
+    %replace '-' with '_':
+    if strcmp(str(k),'-')
+        str(k) = '_';
+    end
+    if strcmp(str(k),'/')||strcmp(str(k),'\')
+        folder = str(k+1:end);
+        break
+    end
+end
+varname = ['summary_',folder];
+eval([varname '=summary;']);
+%save summary:
+save([varname,'.mat'],varname);
+
+
