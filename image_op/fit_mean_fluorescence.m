b413a43f9f18247ecc6069c8f10246438519640d
kimth <kimth@stanford.edu>
2015-02-09 12:01:02 -0800

    Just fit, not normalize

--- image_op/fit_mean_fluorescence.m
+++ image_op/fit_mean_fluorescence.m
@@ -1,22 +1,13 @@
-function M_norm = fit_mean_fluorescence(movie)
-% Fit a sum of two exponentials to the mean flourescence
-%
-%   movie: movie matrix , [h x w x num_frames]
+function F_fit = fit_mean_fluorescence(F)
+% Fit a sum of two exponentials to the mean flourescence F
 %
 % 2015 01 31 Tony Hyun Kim (Revised: Hakan Inan, 15-Jan-4)
 %
 
-F = compute_mean_fluorescence(movie);
 num_frames = length(F);
 
-time = 1/20*(0:(num_frames-1));
+time = 0:(num_frames-1);
 
-f2 = fit(time',F','exp2');%fit sum of 2 exponentials
+f2 = fit(time',F','exp2'); % Fit sum of 2 exponentials
 params = coeffvalues(f2);
 F_fit = params(1)*exp(params(2)*time) + params(3)*exp(params(4)*time);
-
-% Normalize movie
-M_norm = zeros(size(movie), 'single');
-for i = 1:num_frames
-    M_norm(:,:,i) = single(movie(:,:,i))/F_fit(i);
-end
