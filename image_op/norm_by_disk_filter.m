153c59c12244c4136fee3e3c27a695a60d423597
Fori <forea@stanford.edu>
2015-02-23 14:20:24 -0800

    Revisions to disk normalization

--- image_op/norm_by_disk_filter.m
+++ image_op/norm_by_disk_filter.m
@@ -1,10 +1,10 @@
-function M_norm = norm_by_disk_filter(movie,varargin)
+function norm_by_disk_filter(hdf5Dir,hdf5Name,dataset,varargin)
 %Normalize every frame in the movie by a disk filtered version of itself
 %
 %   movie: movie matrix, [h x w x num_frames]
 %   disk_radius may be passed as an input argument. Default for disk_radius
 %   is 15.
-% 2015 01 31 Tony Hyun Kim (Latest Revision: Hakan Inan, 15-Jan-5)
+% 2015 01 31 Tony Hyun Kim (Latest Revision: Jessica Maxey, 15-Feb-23)
 %
 
 if isempty(varargin)
@@ -15,28 +15,23 @@ else
     error('Only 1 variable input argument is allowed');
 end
 
-M_norm = zeros(size(movie), 'single');
-num_frames = size(movie,3);
+h5att = h5info(fullfile(hdf5Dir,hdf5Name),'/Data/Images');
+imageStackSize = h5att.Dataspace.Size;
+rows = imageStackSize(1);
+cols = imageStackSize(2);
+num_frames = imageStackSize(3);
+
+h5create(fullfile(hdf5Dir,hdf5Name),'/Data/Norm',[rows,cols,num_frames],'ChunkSize',[rows,cols,1],'Datatype','single');
 
 % Apply spatial normalization
 hDisk = fspecial('disk', disk_radius);
 
-ref_idx = 1;
-m_f = imfilter(single(movie(:,:,ref_idx)), hDisk, 'replicate');
-m0 = mean(m_f(:));
-imagesc(m_f);
-xlabel('x [px]');
-ylabel('y [px]');
-title(sprintf('Frame %d filtered by disk of radius %d', ref_idx, disk_radius));
-input('norm_by_disk_filter: Press enter to proceed >> ');
-
-for i = 1:num_frames
+for i=1:num_frames
     if (mod(i,1000)==0)
         fprintf('  Frames %d of %d done\n', i, num_frames);
     end
-    m = single(movie(:,:,i));
+    m = single(h5read(fullfile(hdf5Dir,hdf5Name),dataset,[1,1,i],[rows,cols,1]));
     m_f = imfilter(m, hDisk, 'replicate');
-    m1 = mean(m_f(:));
-    m_f = m0/m1*m_f;
-    M_norm(:,:,i) = m./m_f;
+    M_norm = m./m_f;
+    h5write(fullfile(hdf5Dir,hdf5Name),'/Data/Norm',M_norm,[1,1,i],[rows,cols,1]);
 end
