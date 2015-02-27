a0e0fcb11fd599ebcca74c461ff829186ee39a25
Fori <forea@stanford.edu>
2015-02-27 15:53:32 -0800

    Updates to MouseTracker

--- behavior/MouseTracking/get_mouse_XY_pos.m
+++ behavior/MouseTracking/get_mouse_XY_pos.m
@@ -27,33 +27,31 @@ function [ centroids ] = get_mouse_XY_pos( movie )
 
         % read in all of the frames for the trial at the beginning
         frame_range = [frame_indices(idx,1) frame_indices(idx,2)];
-        video = read(behavior_vid,frame_range);
-        image = read(behavior_vid,1);
+        video = read(behavior_vid,frame_range);       
         
-        
-        % plot original image (original movie on the left)
-        subplot(121);
+%         % plot original image (original movie on the left)
+%         subplot(121);
 %         image = video(:,:,:,1);
-        h = imagesc(image);
-        title(sprintf('Original: Frames %d - %d',...
-                      frame_indices(idx,1), frame_indices(idx,2)));
-        axis image; 
-        hold on
-        c_old = [0 0];
+%         h = imagesc(image);
+%         title(sprintf('Original: Frames %d - %d',...
+%                       frame_indices(idx,1), frame_indices(idx,2)));
+%         axis image; 
+%         hold on
+%         c_old = [0 0];
 
-        % plot initial centroids on Original Video
-        k = plot(0,0,'k*');
+%         % plot initial centroids on Original Video
+%         k = plot(0,0,'k*');
 
-        % plot initial Tracker image (on the right)
-        subplot(122);
-        j = imagesc(image);
-        title(sprintf('Tracker: Frames %d - %d',...
-                      frame_indices(idx,1),frame_indices(idx,2)));
-        hold on
-        axis image;
+%         % plot initial Tracker image (on the right)
+%         subplot(122);
+%         j = imagesc(image);
+%         title(sprintf('Tracker: Frames %d - %d',...
+%                       frame_indices(idx,1),frame_indices(idx,2)));
+%         hold on
+%         axis image;
 
-        % plot centroids on Tracker subplot
-        l = plot(0,0,'k*');
+%         % plot centroids on Tracker subplot
+%         l = plot(0,0,'k*');
 
         % set background subtraction image to be first frame
         bg_image = video(:,:,:,frame_indices(idx,2)-(frame_indices(idx,1)-1));
@@ -66,27 +64,27 @@ function [ centroids ] = get_mouse_XY_pos( movie )
 
            % Update original image CData
             image = video(:,:,:,frame_idx);
-            set(h,'CData',image); 
-            pause(0.001);
+%             set(h,'CData',image); 
+%             pause(0.001);
 
             % Find the mouse blob using findMouse helper function
             thresh = 20; % assumes that black mouse has RGB values <20
-            [final_image,s] = findMouse(bg_image,image,thresh);
+            [~,s] = findMouse(bg_image,image,thresh);
             area_vector = [s.Area];
             length_vector = [s.MinorAxisLength];
 
             % Update binarized image
-            set(j,'CData',final_image);
-            pause(0.001);
+%             set(j,'CData',final_image);
+%             pause(0.001);
 
             % Save centroids data and update plot
             if isempty(area_vector) %sometimes blob disappears, go to previous centroid
                 centroids{frame_indices(idx,1)+frame_idx-1} = c_old;
 
-                % Update subplots
-                set(k,'XData',c_old(1),'YData',c_old(2),'color','r');
-                set(l,'XData',c_old(1),'YData',c_old(2),'color','r');
-                pause(0.001);
+%                 % Update subplots
+%                 set(k,'XData',c_old(1),'YData',c_old(2),'color','r');
+%                 set(l,'XData',c_old(1),'YData',c_old(2),'color','r');
+%                 pause(0.001);
 
             else % new centroid
                 [~, id] = max(length_vector); %assume mouse is the fattest blob
@@ -94,10 +92,10 @@ function [ centroids ] = get_mouse_XY_pos( movie )
                 centroids{frame_indices(idx,1)+frame_idx-1} = c_new;
                 c_old = c_new;
 
-                % Update subplots
-                set(k,'XData',c_new(1),'YData',c_new(2),'color','b');
-                set(l,'XData',c_new(1),'YData',c_new(2),'color','b');
-                pause(0.001);
+%                 % Update subplots
+%                 set(k,'XData',c_new(1),'YData',c_new(2),'color','b');
+%                 set(l,'XData',c_new(1),'YData',c_new(2),'color','b');
+%                 pause(0.001);
             end
         end    
     end
