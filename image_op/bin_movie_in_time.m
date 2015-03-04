9a98c78e22ea5b6e04fe68f4bde83c238d7a0e52
kimth <kimth@stanford.edu>
2015-03-04 12:30:02 -0800

    Binning movie -- wip

--- image_op/bin_movie_in_time.m
+++ image_op/bin_movie_in_time.m
@@ -1,40 +1,74 @@
-function M_b = bin_movie_in_time(M, bin_factor, trial_indices)
+function [M_b, binned_indices] = bin_movie_in_time(M, bin_factor, trial_indices)
 % Temporally bin the provided movie M by bin_factor, but do not bin frames
-% across trial boundaries. Non-trial frames, if they exist in the original
-% movie, will be excised.
+% across trial boundaries. Dangling frames will be excised. Furthermore,
+% non-trial frames -- if they exist in the original movie -- will also be
+% excised.
 %
 % Inputs:
 %   - M: Movie to be temporally binned [height x width x num_frames]
 %   - bin_factor: Number of frames in the original movie that will 
 %       correspond to one frame in the binned movie.
-%   - trial_indices: [num_trials x 2] matrix indicating the start and end
-%       frames of each trial with respect to the input movie M.
+%   - trial_indices: [num_trials x 4] matrix, where the i-th row indicates
+%       the frame indices of Trial i as [start open-gate close-gate end]
+%
+% Outputs:
+%   - M_b: Binned movie
+%   - binned_indices: [num_trials x 4] matrix where the i-th row indicates
+%       the frame indices with respect to the binned movie.
 %
 
-[height, width, num_frames] = size(M);
+[height, width, ~] = size(M);
 
-% Compute the number of binned frames for each trial
-frames_per_trial = trial_indices(:,end) - trial_indices(:,1) + 1;
-binned_frames_per_trial = floor(frames_per_trial/bin_factor);
+% Get the binned indices
+binned_indices = bin_frame_indices(trial_indices, bin_factor);
+binned_frames_per_trial = binned_indices(:,end) - binned_indices(:,1) + 1;
 num_binned_frames = sum(binned_frames_per_trial);
 
-fprintf(['  bin_movie_in_time: %d frames from the original movie will be '...
-         'omitted in binning to preserve trial boundaries.\n'],...
-         num_frames - bin_factor*num_binned_frames);
+% Generate the binned movie
+M_b = zeros(height, width, num_binned_frames, 'single');
 
-M_b = 0;
-% M_b = zeros(height, width, num_binned_frames, 'single');
+write_idx = 1;
+for trial_idx = 1:num_trials
+    for k = 1:binned_frames_per_trial(trial_idx)
+        % Indices into the original movie
+        frame_start = trial_indices(trial_idx,1) + bin_factor*(k-1);
+        frame_end   = frame_start + (bin_factor-1);
+        frames = frame_start:frame_end;
+        
+        % Compute the mean
+        M_b(:,:,write_idx) = mean(M(:,:,frames),3);
+        write_idx = write_idx + 1;
+    end
+end
 
-% for k = 1:num_downsampled_frames
-%     if (mod(k, 1000)==0)
-%         fprintf('  Frames %d / %d downsampled\n', k, num_downsampled_frames);
-%     end
-%     
-%     % Compute indices to original movie
-%     frame_start = 1 + bin_factor*(k-1);
-%     frame_end   = frame_start + (bin_factor-1);
-%     frames = frame_start:frame_end;
-%     
-%     % Compute the mean
-%     M_b(:,:,k) = mean(M(:,:,frames),3);
-% end
+end % bin_movie_in_time
+
+function binned_indices = bin_frame_indices(orig_indices, bin_factor)
+% TODO: Replace the globally visible 'io/bin_frame_indices.m' with this
+%   implementation.
+
+[num_trials, K] = size(orig_indices);
+
+frames_per_trial = orig_indices(:,end) - orig_indices(:,1) + 1;
+binned_frames_per_trial = floor(frames_per_trial/bin_factor);
+
+num_frames = orig_indices(num_trials, end);
+num_binned_frames = sum(binned_frames_per_trial);
+fprintf('Note: Omitted %d frames from the original movie by binning',...
+        num_frames - bin_factor*num_binned_frames);
+
+% Explicitly remove dangling frames from the original frame indices
+for trial_idx = 1:num_trials
+    max_index = orig_indices(trial_idx,1) + ...
+                bin_factor * binned_frames_per_trial(trial_idx) - 1;
+    orig_indices(trial_idx,:) = min(orig_indices(trial_idx,:),...
+                                    max_index*ones(1,K)); % Apply clamp
+end
+
+% Generate the binned indices
+binned_start_indices = cumsum([1; binned_frames_per_trial(1:end-1)]);
+binned_offsets = floor(diff(orig_indices,1,2)/bin_factor); % Offsets in each trial
+
+binned_indices = cumsum([binned_start_indices binned_offsets],2);
+
+end
