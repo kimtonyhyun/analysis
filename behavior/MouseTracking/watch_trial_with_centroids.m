16618e0d0aac86a2cb6f211628f7457683bf9aa0
Fori <forea@stanford.edu>
2015-03-06 14:58:19 -0800

    updates

--- behavior/MouseTracking/watch_trial_with_centroids.m
+++ behavior/MouseTracking/watch_trial_with_centroids.m
@@ -39,7 +39,7 @@ function watch_trial_with_centroids( movie, centroids_mat, trial, varargin )
     trial_start = this_trial_indices(1);  
     first_image = read(behavior_vid,trial_start);
     k = imagesc(first_image);
-    title('Trial');
+    title(sprintf('Trial %d',trial(1)),'FontSize',18);
     axis image; colormap gray;
     hold on
     l = plot(centroids(trial_start,1),centroids(trial_start,2),'g*');
@@ -49,29 +49,39 @@ function watch_trial_with_centroids( movie, centroids_mat, trial, varargin )
     for i = 1:length(trial)
         
         % update trial indices
-        title(sprintf('Trial %d',trial(i)));
         this_trial = trial(i);
+        title(sprintf('Trial %d',this_trial));
         this_trial_indices = trial_indices(this_trial,:);
         trial_start = this_trial_indices(1);  
         
         % load trial video
         trial_video = read(behavior_vid,this_trial_indices);
         trial_video = squeeze(trial_video(:,:,1,:)); % 3D movie stack
-
-        % update plot
+       
+        % plot remaining frames
         for frame_idx = 1:size(trial_video,3);
             image = trial_video(:,:,frame_idx);
             this_centroid = centroids(frame_idx+trial_start-1,:);
 
             set(k,'CData',image);
-            set(l,'XData',this_centroid(1),'YData',this_centroid(2));
-            pause(0.1);
+            
+            if frame_idx == 1
+                centroid_color = 'm';
+                pause_time = 1.0; % inspect first frame for bleedthrough
+            else
+                centroid_color = 'g';
+                pause_time = 0.1; % go through rest of movie fast
+            end
+            
+            set(l,'XData',this_centroid(1),'YData',this_centroid(2),'color',centroid_color);
+            pause(pause_time); % play through rest of movie faster
+            
         end
         
         % display black screen at end of each trial
         set(k,'CData',blank_image);
+        set(l,'color','k');
         pause(0.5);
     end
 
 end
-
