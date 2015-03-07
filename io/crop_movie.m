de753ea535ba36051512941c6ff9cc4a793ef697
kimth <kimth@stanford.edu>
2015-03-07 08:14:53 -0800

    Cropping region can be provided programmatically

--- io/crop_movie.m
+++ io/crop_movie.m
@@ -1,6 +1,11 @@
-function crop_movie(movie_in, movie_out)
-% Crop HDF5 movie from file ('movie_in') to file ('movie_out'). The
-% cropping region is defined by the user interactively.
+function crop_movie(movie_in, movie_out, varargin)
+% Crop HDF5 movie from file ('movie_in') to file ('movie_out'). Optional
+% arguments can specify the cropping region programmatically. For example:
+%   - crop_movie(movie_in, movie_out, 'trim', 5);
+% will crop out 5 pixels from the borders of the movie.
+%
+% If no optional arguments are provided, then the cropping region is
+% defined interactively.
 %
 % If 'movie_out' is left as an empty string, then default name will be
 % provided.
@@ -33,25 +38,43 @@ height = h5att.Dataspace.Size(1);
 width = h5att.Dataspace.Size(2);
 num_frames = h5att.Dataspace.Size(3);
 
+% Optional specification of the cropping ROI
+x_bounds = [];
+y_bounds = [];
+if ~isempty(varargin)
+    for k = 1:length(varargin)
+        switch lower(varargin{k})
+            case 'trim' % Trim borders
+                trim = varargin{k+1};
+                x_bounds = [1+trim width-trim];
+                y_bounds = [1+trim height-trim];
+        end
+    end
+end
+
 % Let the user select the crop region
-% TODO: Allow the cropping window to be defined as a parameter to the function
 %------------------------------------------------------------
 ref_idx = 1;
 ref_frame = h5read(movie_in, movie_dataset, [1 1 ref_idx], [height width 1]);
-imagesc(ref_frame);
-colormap gray;
-axis image;
-title(strrep(movie_in, '_', '\_'));
-fprintf('crop_movie: Please provide a rectangular region over the image.\n');
-fprintf('  Double click on the rectangle when done.\n');
-h_rect = imrect;
-rect_params = round(wait(h_rect));
+
+if isempty(x_bounds) % Cropping region was not specified programmatically
+    imagesc(ref_frame);
+    colormap gray;
+    axis image;
+    title(strrep(movie_in, '_', '\_'));
+    fprintf('crop_movie: Please provide a rectangular region over the image.\n');
+    fprintf('  Double click on the rectangle when done.\n');
+    h_rect = imrect;
+    rect_params = round(wait(h_rect));
+    x_bounds = [rect_params(1) rect_params(1)+rect_params(3)-1];
+    y_bounds = [rect_params(2) rect_params(2)+rect_params(4)-1];
+end
 
 % Show the cropped image
-x0 = rect_params(1); x1 = rect_params(1)+rect_params(3)-1;
-y0 = rect_params(2); y1 = rect_params(2)+rect_params(4)-1;
-ref_frame_cropped = ref_frame(y0:y1, x0:x1);
+ref_frame_cropped = ref_frame(y_bounds(1):y_bounds(2),...
+                              x_bounds(1):x_bounds(2));
 imagesc(ref_frame_cropped);
+colormap gray;
 axis image;
 title(sprintf('%s (cropped)',strrep(movie_in, '_', '\_')));
 
@@ -59,8 +82,8 @@ input('crop_movie: Press enter to apply crop to entire movie >> ');
 
 % Prepare output movie
 %------------------------------------------------------------
-cropped_height = rect_params(4);
-cropped_width = rect_params(3);
+cropped_height = y_bounds(2)-y_bounds(1)+1;
+cropped_width = x_bounds(2)-x_bounds(1)+1;
 h5create(movie_out, movie_dataset,...
          [cropped_height cropped_width, num_frames],...
          'ChunkSize', [cropped_height, cropped_width 1],...
@@ -68,8 +91,10 @@ h5create(movie_out, movie_dataset,...
 
 copy_hdf5_params(movie_in, movie_out);
 
-h5create(movie_out, '/Crop/CropRect', [1 4], 'Datatype', 'uint16');
-h5write(movie_out, '/Crop/CropRect', uint16(rect_params));
+h5create(movie_out, '/Crop/XBounds', [1 2], 'Datatype', 'double');
+h5write(movie_out, '/Crop/XBounds', x_bounds);
+h5create(movie_out, '/Crop/YBounds', [1 2], 'Datatype', 'double');
+h5write(movie_out, '/Crop/YBounds', y_bounds);
 
 % Process the remainder of the movie
 frame_chunk_size = 2500;
@@ -87,7 +112,7 @@ for i = 1:num_chunks
                          [height width chunk_count]);
                          
     h5write(movie_out, movie_dataset,...
-            movie_chunk(y0:y1, x0:x1, :),...
+            movie_chunk(y_bounds(1):y_bounds(2), x_bounds(1):x_bounds(2), :),...
             [1 1 chunk_start],...
             [cropped_height cropped_width chunk_count]);
 end
