7d3172a281b73e340b3b8e49bb12ef37344ba859
kimth <kimth@stanford.edu>
2015-03-09 08:52:40 -0700

    Optional arguments to trim borders

--- image_op/compute_fluorescence_stats.m
+++ image_op/compute_fluorescence_stats.m
@@ -1,4 +1,4 @@
-function F = compute_fluorescence_stats(M)
+function F = compute_fluorescence_stats(M, varargin)
 % Compute fluorescence stats (min, mean, max) of a movie M on a 
 % frame-by-frame basis.
 %
@@ -6,14 +6,26 @@ function F = compute_fluorescence_stats(M)
 %   M: [height x width x num_frames] movie to be examined
 %
 
+trim = 0;
+if ~isempty(varargin)
+    for k = 1:length(varargin)
+        switch lower(varargin{k})
+            case 'trim' % Trim borders
+                trim = varargin{k+1};
+        end
+    end
+end
+
 num_frames = size(M,3);
 F = zeros(num_frames, 3);
-for i = 1:num_frames
-    if (mod(i,2500)==0)
+for frame_idx = 1:num_frames
+    if (mod(frame_idx,2500)==0)
         fprintf('%s: Frames %d of %d examined...\n',...
-            datestr(now), i, num_frames);
+            datestr(now), frame_idx, num_frames);
     end
-    m = M(:,:,i);
+    m = M((1+trim):(end-trim),...
+          (1+trim):(end-trim),...
+           frame_idx);
     m = m(:);
-    F(i,:) = [min(m) mean(m) max(m)];
+    F(frame_idx,:) = [min(m) mean(m) max(m)];
 end
