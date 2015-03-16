f3b45779f798de3e06c38a28b10a26a713009103
kimth <kimth@stanford.edu>
2015-03-16 14:30:52 -0700

    DaySummary first implementation

--- utils/get_most_recent_file.m
+++ utils/get_most_recent_file.m
@@ -1,10 +1,13 @@
 function filename = get_most_recent_file(path_to, pattern)
-
 % Usage:
 %   get_most_recent_file('ica001', 'ica_*.mat')
 
 files = dir(fullfile(path_to, pattern));
-datenums = files.datenum;
-[~, sorted] = sort(datenums);
 
-filename = fullfile(path_to, files(sorted(1)).name);
+filename = '';
+if ~isempty(files)
+    datenums = files.datenum;
+    [~, sorted] = sort(datenums);
+
+    filename = fullfile(path_to, files(sorted(1)).name);
+end
