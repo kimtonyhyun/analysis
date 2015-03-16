0ed45a0b5dbdb60e113d6e5ae9c3c6ff5202f97e
kimth <kimth@stanford.edu>
2015-03-16 14:41:02 -0700

    Minor code juggling and add accessor

--- io/DaySummary.m
+++ io/DaySummary.m
@@ -26,15 +26,7 @@ classdef DaySummary
     
     methods
         function obj = DaySummary(plusmaze_txt, ica_dir, varargin)
-            % Load data
-            %------------------------------------------------------------
-            [trial_indices, loc_info, trial_durations] =...
-                parse_plusmaze(plusmaze_txt);
-            
-            data = load(get_most_recent_file(ica_dir, 'ica_*.mat'));
-            
-            % Parse trial data
-            %------------------------------------------------------------
+            % Handle optional input
             exclude_probe_trials = 0;
             for k = 1:length(varargin)
                 switch lower(varargin{k})
@@ -43,6 +35,16 @@ classdef DaySummary
                 end
             end
             
+            % Load data
+            %------------------------------------------------------------
+            [trial_indices, loc_info, trial_durations] =...
+                parse_plusmaze(plusmaze_txt);
+            
+            data = load(get_most_recent_file(ica_dir, 'ica_*.mat'));
+            
+            % Parse trial data
+            %   TODO: Bring in centroids corresponding to mouse position
+            %------------------------------------------------------------
             if (exclude_probe_trials)
                 is_probe = strcmp(loc_info(:,1), 'north') | ...
                            strcmp(loc_info(:,1), 'south');
@@ -52,15 +54,16 @@ classdef DaySummary
                 trial_durations = trial_durations(~is_probe);
             end
             
-            obj.trial_indices = trial_indices;
-            obj.num_trials = size(trial_indices, 1);
-            traces = cell(obj.num_trials, 1);
-            for k = 1:obj.num_trials
+            num_trials = size(trial_indices, 1);
+            traces = cell(num_trials, 1);
+            for k = 1:num_trials
                 trial_frames = trial_indices(k,1):...
                                trial_indices(k,end);
                 traces{k} = data.ica_traces(trial_frames, :)';
             end
             
+            obj.num_trials = num_trials;
+            obj.trial_indices = trial_indices;
             obj.trials = struct(...
                 'start', loc_info(:,1),...
                 'goal',  loc_info(:,2),...
@@ -76,6 +79,12 @@ classdef DaySummary
             obj.cells = struct(...
                 'im', squeeze(num2cell(data.ica_filters, [1 2])),...
                 'label', class);
+        end
+        
+        function [trace, frame_indices] = get_cell_trial(obj, cell_idx, trial_idx)
+            trace = obj.trials(trial_idx).traces(cell_idx,:);
+            frame_indices = obj.trial_indices(trial_idx,1):...
+                            obj.trial_indices(trial_idx,end);
         end
     end
 end
