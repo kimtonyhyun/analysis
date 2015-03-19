3adfe584944ceab969183f0bf72bb356587730e9
kimth <kimth@stanford.edu>
2015-03-19 13:44:27 -0700

    Split ROIs

--- image_op/reconstruct_traces.m
+++ image_op/reconstruct_traces.m
@@ -53,7 +53,90 @@ if ~isempty(varargin)
     end
 end
 
-% Load data
+% Load ICA
+ica_filename = get_most_recent_file(ica_dir, 'ica_*.mat');
+ica = load(ica_filename);
+[height, width, num_ICs] = size(ica.filters);
+
+% Reconstruction settings
+info.type = 'reconstruction';
+info.movie_source = movie_source;
+info.ica_source = ica_filename;
+
+info.threshIC = threshIC;
+info.threshMov = threshMov;
+
+% Build reconstruction filters
+%------------------------------------------------------------
+filters = zeros(height, width, 2*num_ICs, 'single'); % Preallocate buffer
+fprintf('%s: Thresholding IC filters...\n', datestr(now));
+
+rec_filter_count = 0;
+for ic_idx = 1:num_ICs
+    ic_filter = ica.filters(:,:,ic_idx);
+    [boundaries, ic_mask] = compute_ic_boundary(ic_filter, threshIC);
+    num_boundaries = length(boundaries);
+    if (num_boundaries == 1) % Only 1 ROI detected
+        rec_filter_count = rec_filter_count + 1;
+        filters(:,:,rec_filter_count) = ic_filter .* ic_mask;
+    else % Multiple ROIs detected
+        submasks = zeros(height, width, num_boundaries);
+        
+        % Show the multiple ROIs to the user
+        imagesc(ic_filter .* ic_mask);
+        hold on;
+        for k = 1:num_boundaries
+            boundary = boundaries{k};
+            plot(boundary(:,1), boundary(:,2), 'r', 'LineWidth', 2);
+            text(max(boundary(:,1)), min(boundary(:,2)),...
+                 num2str(k),...
+                 'Color', 'w');
+             
+            submasks(:,:,k) = poly2mask(boundary(:,1), boundary(:,2), height, width);
+        end
+        hold off;
+        
+        title(sprintf('%s -- IC %d', strrep(ica_filename, '_', '\_'), ic_idx));
+        axis image;
+        drawnow;
+        
+        % Get list of ROIs to keep
+        fprintf('%s: Please select ROIs to keep from IC %d\n', datestr(now), ic_idx);
+        sel_rois = [];
+        val = str2double(strtrim(input('  >> ','s')));
+        while (1)
+            if ~isnan(val) % Is a number
+                if ((1 <= val) && (val <= num_boundaries))
+                    if ismember(val, sel_rois)
+                        sel_rois = setdiff(sel_rois, val);
+                    else
+                        sel_rois = union(sel_rois, val);
+                    end
+                else
+                    fprintf('    Sorry, %d is not a valid ROI index\n', val);
+                end
+            else % Not a number -- done with ROI selection
+                for sel_roi = sel_rois
+                    rec_filter_count = rec_filter_count + 1;
+                    filters(:,:,rec_filter_count) = ic_filter .* submasks(sel_roi);
+                    fprintf('    Added ROI %d from IC %d\n', sel_roi, ic_idx);
+                end
+                break; % Get out of selection loop
+            end
+            
+            val = str2double(strtrim(input('  >> ','s')));
+        end
+    end
+end
+info.num_pairs = rec_filter_count; %#ok<STRNU>
+filters = filters(:,:,1:rec_filter_count);
+
+for cell_idx = 1:rec_filter_count % Normalize
+    filter = filters(:,:,cell_idx);
+    filters(:,:,cell_idx) = filter / sum(filter(:));
+end
+
+% Reconstruct traces
 %------------------------------------------------------------
 fprintf('%s: Loading movie...\n', datestr(now));
 M = load_movie(movie_source);
@@ -68,64 +151,14 @@ elseif threshMov>0
     threshMov = -threshMov*maxMov;
 end
 
-% Load ICA
-ica_filename = get_most_recent_file(ica_dir, 'ica_*.mat');
-ica = load(ica_filename);
-[height, width, num_ICs] = size(ica.filters);
-
-% Compute the reconstruction
-%------------------------------------------------------------
-info.type = 'reconstruction';
-info.movie_source = movie_source;
-info.ica_source = ica_filename;
-
-info.threshIC = threshIC;
-info.threshMov = threshMov;
-
-filters = zeros(height, width, 2*num_ICs, 'single'); % Preallocate
-fprintf('%s: Thresholding IC filters...\n', datestr(now));
-
-rec_filter_count = 0;
-for idx_cell = 1:num_ICs
-    ic_filter = ica.filters(:,:,idx_cell);
-    [boundaries, ic_mask] = compute_ic_boundary(ic_filter, threshIC);
-    num_boundaries = length(boundaries);
-    if (num_boundaries == 1) % Only 1 ROI detected
-        rec_filter_count = rec_filter_count + 1;
-        filters(:,:,rec_filter_count) = ic_filter .* ic_mask;
-    else % Multiple ROIs detected -- prompt user for split
-        imagesc(ic_filter .* ic_mask);
-        hold on;
-        for k = 1:num_boundaries
-            boundary = boundaries{k};
-            plot(boundary(:,1), boundary(:,2), 'r', 'LineWidth', 2);
-            text(max(boundary(:,1)), min(boundary(:,2)),...
-                 num2str(k),...
-                 'Color', 'w');
-        end
-        hold off;
-        
-        title(sprintf('%s -- IC %d', strrep(ica_filename, '_', '\_'), idx_cell));
-        axis image;
-        pause;
-    end
-end
-info.num_pairs = rec_filter_count; %#ok<STRNU>
-filters = filters(:,:,1:rec_filter_count);
-
-for idx_cell = 1:rec_filter_count % Normalize
-    filter = filters(:,:,idx_cell);
-    filters(:,:,idx_cell) = filter / sum(filter(:));
-end
-    
 traces = zeros(num_frames, rec_filter_count, 'single');
 fprintf('%s: Reconstructing traces...\n', datestr(now));
-for idx_cell = 1:rec_filter_count
-    rec_filter = filters(:,:,idx_cell);
+for cell_idx = 1:rec_filter_count
+    rec_filter = filters(:,:,cell_idx);
     pix_active = find(rec_filter>0);
     movie_portion = M(pix_active,:)';
     movie_portion(movie_portion<threshMov) = 0;
-    traces(:,idx_cell) = movie_portion * rec_filter(pix_active);  
+    traces(:,cell_idx) = movie_portion * rec_filter(pix_active);  
 end
 
 % Save the result to mat file
@@ -136,4 +169,3 @@ rec_savename = sprintf('rec_%s.mat', timestamp);
 save(rec_savename, 'info', 'filters', 'traces');
 
 fprintf('%s: Done!\n', datestr(now));
-
