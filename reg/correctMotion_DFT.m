1c09c97227594c0788cc4f199faa962d3d500402
jmaxey <jmaxey@stanford.edu>
2015-03-23 11:24:04 -0700

    Made corrections to correctMotion_DFT

--- reg/correctMotion_DFT.m
+++ reg/correctMotion_DFT.m
@@ -1,13 +1,24 @@
-function correctMotion_DFT(hdf5_dir,hdf5_in_name,hdf5_out_name)
+function correctMotion_DFT(hdf5_in_name,hdf5_out_name)
+
+% correctMotion_DFT performs motion registration using the DFT method. Each
+% frame in the input file is registered to the first frame and saved in an
+% hdf5.
+%
+% Inputs: 
+%   hdf5_in_name: Name of input hdf5 file (including leading directory.
+% Outputs:
+%   hdf5_out_name: Name of output hdf5 file (including leading directory).
+%
+% Jessica Maxey, March 23, 2015
 
 dataset = '/Data/Images';
 
-h5att = h5info(fullfile(hdf5_dir,hdf5_in_name),dataset);
+h5att = h5info(hdf5_in_name,dataset);
 imageStackSize = h5att.Dataspace.Size;
 rows = imageStackSize(1);
 cols = imageStackSize(2);
 frames = imageStackSize(3);
-baseImage = h5read(fullfile(hdf5_dir,hdf5_in_name),dataset,[1,1,1],[rows,cols,1]);
+baseImage = h5read(hdf5_in_name,dataset,[1,1,1],[rows,cols,1]);
 
 ssm_radius = 20;
 asm_radius = 5;
@@ -25,20 +36,24 @@ imagesc(im_ref); axis image; colormap gray;
 title('Select ROI');
 h_rect = imrect;
 mask_rect = round(getPosition(h_rect));
-cropBase = im_ref(mask_rect(2):mask_rect(2)+mask_rect(4),mask_rect(1):mask_rect(1)+mask_rect(3));
+startRow = mask_rect(2);
+stopRow = mask_rect(2)+mask_rect(4);
+startCol = mask_rect(1);
+stopCol = mask_rect(1)+mask_rect(3);
+cropBase = im_ref(startRow:stopRow,startCol:stopCol);
 close(h_roi);
 
 baseFFT = fft2(cropBase);
 
 chunkSize = [rows,cols,1];
-h5create(fullfile(hdf5_dir,hdf5_out_name),dataset,[Inf,Inf,Inf],'ChunkSize',chunkSize,'Datatype','single');
-h5write(fullfile(hdf5_dir,hdf5_out_name),dataset,baseImage,[1,1,1],[rows,cols,1]);
+h5create(hdf5_out_name,dataset,[Inf,Inf,Inf],'ChunkSize',chunkSize,'Datatype','single');
+h5write(hdf5_dir,hdf5_out_name,dataset,baseImage,[1,1,1],[rows,cols,1]);
 
 h = waitbar(0,'Motion Correction in Progress...');
 for f=2:frames
     waitbar(f/frames);
-    image = h5read(fullfile(hdf5_dir,hdf5_in_name),dataset,[1,1,f],[rows,cols,1]);
-    cropImage = image(mask_rect(2):mask_rect(2)+mask_rect(4),mask_rect(1):mask_rect(1)+mask_rect(3));
+    image = h5read(hdf5_in_name,dataset,[1,1,f],[rows,cols,1]);
+    cropImage = image(startRow:stopRow,startCol:stopCol);
     tImage = transform(cropImage);
     imageFFT = fft2(tImage);
     [output, result] = dftregistration(baseFFT,imageFFT,100); 
@@ -53,7 +68,7 @@ for f=2:frames
     regImageFFT = iFFT*exp(i*output(2));
     regImage = abs(ifft2(regImageFFT));
         
-    h5write(fullfile(hdf5_dir,hdf5_out_name),dataset,regImage,[1,1,f],[rows,cols,1]);
+    h5write(hdf5_out_name,dataset,regImage,[1,1,f],[rows,cols,1]);
 end
 close(h);
 end
