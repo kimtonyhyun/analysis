52e8d2a64f16faf22c1cde3e37ac886fa3130b04
inanhkn <inanh@stanford.edu>
2015-07-01 16:13:46 -0700

    major changes in event detection

--- signal_op/calc_localmax_above_threshold.m
+++ signal_op/calc_localmax_above_threshold.m
@@ -1,27 +1,42 @@
 function localmax = calc_localmax_above_threshold(threshold,data)
-%function calc_localmax_above_threshold(threshold,data)
-%
-%find the local maxima in a sequence of points provided that the maxima are 
+
+% Find the local maxima in a sequence of points provided that the maxima are 
 %above a specified threshold
-%
-%Hakan Inan (Jan 15)
-%
-data_binary = data>threshold; %binarize
-%vector to contain local maxima
+
+% Make data a row vector
+if size(data,2)==1, data = data'; end
+
+thresh_dist = 5; % minimum distance required between events
+data_binary = data>threshold; % Binarize
+
 localmax = [];
+
 k=1;
 while k<length(data_binary)
-    if data_binary(k)==1 % start a sequence of 1's
+    if data_binary(k)==1 % Start a sequence of 1's
+        
         begin_seq = k; 
         while (data_binary(k)==1)&&(k<length(data_binary)), k=k+1; end
         end_seq = k-1;
-        %after the end of sequence calculate max and add it to localmax
-        seq = data(begin_seq:end_seq); %magnitudes of the events in the sequence
-        [maxval,dum] = max(seq);
-        idx_max = dum+begin_seq-1;
-        if maxval>threshold*1.1 % reject very small peaks
-            localmax = [localmax,idx_max]; %#ok
-        end        
+        
+        % After the end of sequence calculate local maxima in it
+        
+        seq = data(begin_seq:end_seq); % Magnitudes of the events in the sequence
+        idx_localmax = find(conv(seq,[-1,2,-1],'valid'))+1; % Indices of local maxima
+        
+        for i = idx_localmax % Check the quality of each local maxima
+            dists = abs(find(seq>seq(i))-i);
+            dists = setdiff(dists,0); % Exclude self
+            min_dist = min(dists);
+            
+            if isempty(dists)
+                localmax = [localmax,i+begin_seq-1]; %#ok
+            elseif min_dist>thresh_dist % Impose min_dist requirement
+                localmax = [localmax,i+begin_seq-1]; %#ok
+            end
+            
+        end
+
     else %skip through 0's
         k = k+1;
     end
--- signal_op/extract_events.m
+++ signal_op/extract_events.m
@@ -1,195 +1,71 @@
-function event_times = extract_events(trace,varargin)
-% event_times = extract_events(trace,varargin)
+function idx_events = extract_events(trace,varargin)
+
+% Extract local maxima in the trace as events
 %
-% Extract event times from the ICA trace of a cell doing 2 iterations
+% trace: vector of fluroescence values to event-detect from
 %
-%   trace : ica trace of a single cell 
-%   An additional inout argument set to 1 will generate plots 
-% 
-%   event_times : [# of events] x 1 array 
+% Variable input arguments:
 %
-% Hakan Inan (Mar 15)
+% 'visualize' : add it as an argument to plot the output
+% 'mad_scale' : enter a numeric value following it to set custom scale for
+%               the mad (median absolute deviation) of the trace for 
+%               determining an event threshold
+%
+% Output:
+% idx_events: indices containing the detected events
 %
 
-%%%%
-%some parameters that can be changed within the script (advanced):
-%%%%
-%mad for df/f trace is multiplied with below to get the coarse threshold
-coarse_thresh_multiplier = 9;
-%mad for deconvolved trace is multiplied with below to get the fine threshold
-fine_thresh_multiplier = 15;
-%offset of the impulse for deconvolution
-offset = 0;
+% Defaults
+mad_scale = 8; 
+do_plot = 0;
 
-% Length before and after the maximum of the characteristic event, h
-num_frames_before_max = 20;
-num_frames_after_max = 100;
-
-idx_event_frames = extract_candidate_events(trace,coarse_thresh_multiplier,0);
-out = iterate_event_indices(idx_event_frames);
-idx_event_frames_v2 = out{1};h_1stround = out{2};g_1ststep = out{3};
-if length(idx_event_frames_v2) < length(idx_event_frames)/3
-    event_times = idx_event_frames;
-    g_final = 1;
-    terminated = 0;
-else
-    out = iterate_event_indices(idx_event_frames_v2);
-    idx_event_frames_v3 = out{1};h_2ndround = out{2};g_2ndstep = out{3};
-    if length(idx_event_frames_v3) < length(idx_event_frames_v2)*2/3
-        event_times = idx_event_frames;
-        g_final = g_1ststep;
-        terminated = 1;
-    else
-        event_times = idx_event_frames_v3;
-        g_final = g_2ndstep;
-        terminated = 2;
-    end
-end
-
-%%%%%%%%%%%%%%
-%visualization
-%%%%%%%%%%%%%%
-fprintf('the algorithm terminated in %d iteration \n',terminated)
 if ~isempty(varargin)
-    if varargin{1}==1
-        if terminated ~=0
-            figure,
-            plot(h_1stround,'k');
-            legend('the characteristic event');
-            if terminated ==2
-                hold on
-                plot(h_2ndround,'r');
-                hold off
-                legend('the characteristic event, 1st iteration','the characteristic event, 2nd iteration');
-                xlabel('frame')
-            end
-        end
-
-        trace = trace / max(trace);
-        mad_scale_coarse = compute_mad(trace);
-        threshold_coarse = coarse_thresh_multiplier*mad_scale_coarse;
-        spikes_vec_coarse = zeros(1,length(trace));
-        spikes_vec_coarse(idx_event_frames) = 1;
-        
-        filtered =  conv(trace,g_final,'same');
-        filtered = filtered / max(filtered);
-        mad_scale_fine = compute_mad(filtered);
-        threshold_fine = fine_thresh_multiplier*mad_scale_fine;
-        spikes_vec_fine = zeros(1,length(trace));
-        spikes_vec_fine(event_times) = 1;
-        
-
-        figure,
-        plot(trace,'k');
-        hold on
-        plot(filtered,'m');
-        plot(1:length(trace),repmat(threshold_coarse,1,length(trace)),'--b');%coarse threshold
-        stem(spikes_vec_coarse*threshold_coarse);
-        legend('raw trace','coarse threshold')
-        if terminated ~=0
-            plot(1:length(trace),repmat(threshold_fine,1,length(trace)),'--r');%fine threshold
-            stem(spikes_vec_fine*threshold_fine);
-            legend('raw trace','coarse threshold','deconvolved trace','deconv threshold')
-        end            
-        hold off
-        xlabel('frame')
-        ylabel('a.u.')
-        title('raw and deconvolved ica traces together with the thresholds and the detected events')
-        
-    end
-end
-
-%%%%%%%%%%%%%%%%%%%
-%internal functions
-%%%%%%%%%%%%%%%%%%%
-function out = iterate_event_indices(idx_event_frames_in)
-
-    h_len = num_frames_before_max+num_frames_after_max+1;
-    event_mat = construct_events(idx_event_frames_in);
-    %calculate the mean of all the trials in the event_mat discarding zeros
-    h = zeros(h_len,1);
-    for k = 1:h_len
-        dum = event_mat(:,k);
-        nonzeros = sum(dum~=0);
-        if nonzeros>=1
-            h(k) = sum(dum)/nonzeros;
+    for k = 1:length(varargin)
+        switch varargin{k}
+            case 'mad_scale'
+                mad_scale = varargin{k+1};
+                if ~isnumeric(mad_scale)
+                    error('Mad scale must be numerical');
+                end
+            case 'visualize'
+                do_plot = 1;
         end
     end
-    %optional: smooth h before inverting
-    SGfilter = SGsmoothingfilter(20,8);
-    h = conv(h,SGfilter,'same');
-    g_len= h_len; %try to find an inverse g that has the same length as h
-    g = invert_h(h,g_len); %invert h 
-    %convolve g with the trace of the whole day and use it to calculate an
-    %adaptive threshold for detecting events
-    trace_filtered = conv(trace,g,'same');
-    idx_event_frames_out = extract_candidate_events(trace_filtered,fine_thresh_multiplier,0);
-    out = {idx_event_frames_out,h,g};
 end
 
-function event_mat = construct_events(idx_event_frames)
+num_frames = length(trace);
+smooth_trace = medfilt1(trace,5);
+mad = compute_mad(smooth_trace);
+thresh = mad_scale * mad;
 
-%align all the trial segments(where there is an event) and accumulate all of them.
-%The events in event_mat are of fixed length with frames before the maximum and
-%frames after the maximum determind by the function inputs.
-%If an event is not long enough on either side, use all the frames to the
-%end on the respective side and append zeros.
+idx_events = calc_localmax_above_threshold(thresh,smooth_trace);
+val_events = zeros(1,length(idx_events));
 
-    h_len = num_frames_before_max+num_frames_after_max+1;
-    event_mat = zeros(length(idx_event_frames),h_len);
+%go back to the original trace and spot the exact maxima
+for k = 1:length(idx_events)
+    coarse_event = idx_events(k);
+    go_back = min(5,coarse_event-1);
+    go_forward = min(5,num_frames-coarse_event);
+    [maxval,idx_max] = max(trace((coarse_event-go_back):(coarse_event+go_forward)));
+    idx_events(k) = idx_max+coarse_event-go_back-1;
+    val_events(k) = maxval;
+end
 
-    for k = 1:length(idx_event_frames) 
+% Remove duplicates (may happen because of median filtering)
+[idx_events,idx_unique] = unique(idx_events);
+val_events = val_events(idx_unique);
 
-        trace_len = length(trace);
-        idx_max = idx_event_frames(k);
+if do_plot    
+    spikes_vec = zeros(1,length(smooth_trace));        
+    spikes_vec(idx_events) = val_events;
 
-        if k<length(idx_event_frames) % k is not the last event
-            num_frames_to_next = idx_event_frames(k+1)-idx_max-1;
-            numframes2add_after = max(min(num_frames_after_max,num_frames_to_next-10),1);
-        else % k is the last event
-            numframes2add_after = min(trace_len-idx_max,num_frames_after_max);
-        end
-        if k>1 % k is not the first event
-            num_frames_from_previous = idx_max-idx_event_frames(k-1);
-            numframes2add_before = max(min(num_frames_before_max,num_frames_from_previous-10),1);
-        else % k is the first event
-            numframes2add_before = min(idx_max-1,num_frames_before_max);
-        end
-
-        indices_trace = (-numframes2add_before:numframes2add_after)+idx_max;
-        indices_event = (-numframes2add_before:numframes2add_after) + num_frames_before_max+1;
-
-        event_to_add = trace(indices_trace);
-        event_mat(k,indices_event) = event_to_add/max(event_to_add);
+    plot(smooth_trace);
+    hold on;
+    stem(spikes_vec);
+    plot(ones(1,length(smooth_trace))*thresh,'r')
+    plot(trace,'--m');
+    hold off
+    for k = 1:length(idx_events)
+        text(idx_events(k),double(thresh*1.1),num2str(k))
     end
-
-end
-
-function g = invert_h(h,g_len)
-    h_len = numel(h);
-    Mconv =  toeplitz([h;zeros(g_len-1,1)],zeros(1,g_len));
-    t_desired = floor(g_len/2)+num_frames_before_max+offset;
-    y = zeros(h_len+g_len-1,1);
-    y(t_desired) = 1;
-
-    % Append noise to A for stability
-    noise_length = ceil(h_len)*4;
-    noise = wgn(noise_length,g_len,0)*0.4; 
-
-    A = [Mconv;noise];
-    y = [y;zeros(noise_length,1)];
-    
-%     Below commented out portion does a better job at filter inversion,
-%     however runs slower and requires CVX package
-%     A_not = A([1:(t_desired-1),(t_desired+1):end],:);
-%     cvx_begin
-%         variable g(g_len,1)
-%         minimize(norm(A_not*g,1))
-%         subject to
-%             A(t_desired,:)*g == 1;
-%     cvx_end
-
-    g = pinv(A)*y;
-end
-
 end
