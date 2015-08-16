78ed70c9b56fffef80ad260401b83a82bea5126c
kimth <kimth@stanford.edu>
2015-08-15 18:19:29 -0700

    Add single cell raster viewer

---
+++ view/view_detailed_raster.m
@@ -0,0 +1,90 @@
+function view_detailed_raster(ds, cell_idx)
+% Plot a detailed "raster summary" of a single cell in DaySummary
+%
+% Usage:
+%     m3d15 = DaySummary(sources.maze, 'rec001', 'reconst', 'excludeprobe');
+%       14-Aug-2015 13:58:23: Loaded data from rec001/rec_150813-115003.mat
+%       14-Aug-2015 13:58:23: Loaded classification from rec001/class_150813-115320.txt
+%     browse_rasters(m3d15);
+%
+
+while (1)
+    clf;
+    draw_rasters();
+    
+    % Ask user for command
+    prompt = sprintf('Cell %d raster >> ', cell_idx);
+    resp = strtrim(input(prompt, 's'));
+    
+    val = str2double(resp);
+    if (~isnan(val)) % Is a number (trial index)
+        
+    else
+        resp = lower(resp);
+        switch (resp)
+            case 't' % "Take" screenshot
+                savename = sprintf('raster_%d.png', cell_idx);
+                print('-dpng', savename);
+                fprintf('  Saved raster as "%s"!\n', savename);
+            
+            case {'q', ''} % Exit
+                break;
+                
+            otherwise
+                fprintf('  Could not parse "%s"\n', resp);
+        end
+    end
+end
+
+    % Helper functions
+    %------------------------------------------------------------
+    function draw_rasters()
+        % Image of cell
+        subplot(3,4,[1 2]);
+        imagesc(ds.cells(cell_idx).im);
+        axis image;
+        title(sprintf('Cell %d (%s)', cell_idx, ds.cells(cell_idx).label));
+        
+        % Raster of all trials, with correctness
+        subplot(3,4,[5 6 9 10]);
+        ds.plot_cell_raster(cell_idx);
+        title('All trials');
+        
+        corr_width = 0.025;
+        xlim([0 1+corr_width]);
+        for i = 1:ds.num_trials
+            if ds.trials(i).correct
+                corr_color = 'g';
+            else
+                corr_color = 'r';
+            end
+            rectangle('Position', [1 i-0.5 corr_width 1],...
+                      'FaceColor', corr_color);
+        end
+        
+        % Divide rasters by correctness
+        subplot(3,4,3);
+        ds.plot_cell_raster(cell_idx, 'correct');
+        title('Correct');
+        subplot(3,4,4);
+        ds.plot_cell_raster(cell_idx, 'incorrect');
+        title('Incorrect');
+        
+        % Divide rasters by start location
+        subplot(3,4,7);
+        ds.plot_cell_raster(cell_idx, 'start', 'west');
+        title('West start');
+        subplot(3,4,8);
+        ds.plot_cell_raster(cell_idx, 'start', 'east');
+        title('East start');
+        
+        % Divide rasters by end location
+        subplot(3,4,11);
+        ds.plot_cell_raster(cell_idx, 'end', 'south');
+        title('South end');
+        subplot(3,4,12);
+        ds.plot_cell_raster(cell_idx, 'end', 'north');
+        title('North end');
+    end % draw_rasters
+
+end % view_cell_rasters
