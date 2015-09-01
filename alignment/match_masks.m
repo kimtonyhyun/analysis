bdf93085230fe2bac32b659df1e67f3c88cd65dc
kimth <kimth@stanford.edu>
2015-08-31 18:17:43 -0700

    Continue refactoring run_alignment

---
+++ alignment/match_masks.m
@@ -0,0 +1,46 @@
+function [match_1to2, match_2to1, M] = match_masks(masks1, masks2)
+% Computes the overlaps between masks1 and masks2.
+
+% Matching parameters
+%------------------------------------------------------------
+min_overlap_threshold = 1/3;
+
+% Compute the matrix of mask overlaps
+%------------------------------------------------------------
+num_masks = [length(masks1) length(masks2)];
+M = zeros(num_masks);
+for i = 1:num_masks(1)
+    if (mod(i,20)==0)
+        fprintf('%s: Computing overlaps (%.1f%%)...\n',...
+            datestr(now), 100*i/num_masks(1));
+    end
+    for j = 1:num_masks(2)
+        M(i,j) = compute_mask_overlap(masks1{i}, masks2{j});
+    end
+end
+fprinft('%s: Overlap matrix completed!\n', datestr(now));
+
+% Find the nonzero elements of the mask overlap matrix
+%------------------------------------------------------------
+match_1to2 = find_nonzero_overlaps(M, min_overlap_threshold);
+match_2to1 = find_nonzero_overlaps(M', min_overlap_threshold);  
+
+end % match_masks
+
+function match = find_nonzero_overlaps(M, threshold)
+
+num_to_match = size(M,1);
+match = cell(num_to_match,1);
+for i = 1:num_to_match
+    js = find(M(i,:) > threshold); % Col indices above threshold
+    if isempty(js)
+        match{i} = [];
+    else
+        overlaps = M(i,js);
+        match{i} = [js' overlaps'];
+        match{i} = sortrows(match{i}, 2); % Sort by overlap value
+        match{i} = flipud(match{i}); % Descending
+    end
+end
+
+end % find_nonzero_overlaps
--- alignment/run_alignment.m
+++ alignment/run_alignment.m
@@ -36,59 +36,16 @@ end
 fprintf('run_alignment: Beginning alignment...\n');    
 [affine_info, masks1, masks2_tform] = compute_affine_transform(ds1, ds2);
 
-% Compute matrix of mask overlaps
-%   Note that the overlap matrix is non-symmetric!
-%------------------------------------------------------------
 input('run_alignment: Press any key to continue with mask matching >> ');
-M = zeros(affine_info.num_cells1, affine_info.num_cells2);
-for i = 1:affine_info.num_cells1
-    if (mod(i,20)==0)
-        fprintf('  %s: Computing overlaps (%.1f%%)...\n',...
-            datestr(now), 100*i/affine_info.num_cells1);
-    end
-    for j = 1:affine_info.num_cells2
-        M(i,j) = compute_mask_overlap(masks1{i}, masks2_tform{j});
-    end
-end
-fprintf('  %s: Overlap matrix completed!\n', datestr(now));
 
-% Find the nonzero elements of the mask overlap matrix
-%------------------------------------------------------------
-overlap_threshold = 1/3;
-match_1to2 = cell(size(M,1),1);
-for i = 1:size(M,1)
-    m = M(i,:);
-    matching_js = find(m>overlap_threshold);
-    if isempty(matching_js)
-        match_1to2{i} = [];
-    else
-        matching_overlaps = m(matching_js);
-        match_1to2{i} = [matching_js' matching_overlaps']; % [j overlap]
-        match_1to2{i} = sortrows(match_1to2{i},2); % Sort by overlap
-        match_1to2{i} = flipud(match_1to2{i}); % Descending
-    end
-end
-
-match_2to1 = cell(size(M,2),1); % Same in the opposite direction
-for j = 1:size(M,2)
-    m = M(:,j)';
-    matching_is = find(m>overlap_threshold);
-    if isempty(matching_is)
-        match_2to1{j} = [];
-    else
-        matching_overlaps = m(matching_is);
-        match_2to1{j} = [matching_is' matching_overlaps'];
-        match_2to1{j} = sortrows(match_2to1{j},2);
-        match_2to1{j} = flipud(match_2to1{j});
-    end
-end
+% Compute matrix of mask overlaps
+[match_1to2, match_2to1, M] = match_masks(masks1, masks2_tform);
 
 % Set up auxiliary output
 %------------------------------------------------------------
 info.affine = affine_info;
 info.masks1 = masks1;
 info.masks2 = masks2_tform;
-info.overlap_threshold = overlap_threshold;
 info.overlap_matrix = M;
 
 % Optional bijective filtering
