bda6d43e1b34aae7294da82e82749acc327c528f
kimth <kimth@stanford.edu>
2015-09-07 10:45:53 -0700

    Compute alignments by graph connected components

--- ds/@MultiDay/MultiDay.m
+++ ds/@MultiDay/MultiDay.m
@@ -3,6 +3,7 @@
 classdef MultiDay < handle
     properties
         valid_days % List of days in the MultiDay object
+        num_days
         num_cells % Number of aligned cells through all days
         matched_indices
     end
@@ -18,7 +19,9 @@ classdef MultiDay < handle
             % Unpack the provided list of DaySummary's into a full cell
             % TODO: Consider using sparse internal storage.
             %------------------------------------------------------------
-            obj.valid_days = cell2mat(ds_list(:,1))';            
+            obj.valid_days = cell2mat(ds_list(:,1))';
+            obj.num_days = length(obj.valid_days);
+            
             max_day = max(obj.valid_days);
 
             obj.full_to_sparse = zeros(1, max_day);
@@ -58,7 +61,7 @@ classdef MultiDay < handle
             end
             
             % Compute matches
-            M = obj.compute_all_matches();
+            M = obj.compute_all_matches_by_graph();
             
             % Filter out rows of M with unmatched indices (i.e. zeros) and
             % store result
@@ -104,23 +107,75 @@ classdef MultiDay < handle
     end
     
     methods (Access=private)
+        function [M, assignments] = compute_all_matches_by_graph(obj)
+            % Cells of all days will be arranged linearly for the graph
+            % computation.
+            num_cells_per_day = zeros(1, obj.num_days);
+            for k = 1:obj.num_days
+                day = obj.valid_days(k);
+                num_cells_per_day(k) = obj.ds{day}.num_cells;
+            end
+            num_all_cells = sum(num_cells_per_day);
+            offsets = cumsum([0 num_cells_per_day(1:(end-1))]);
+
+            % Adjacency matrix with all cells arranged linearly
+            A = zeros(num_all_cells, num_all_cells);
+            for day_i = obj.valid_days
+                for day_j = setdiff(obj.valid_days, day_i)
+                    m_itoj = obj.match{day_i, day_j};
+                    if ~isempty(m_itoj)
+                        for cell_i = 1:length(m_itoj)
+                            m = m_itoj{cell_i};
+                            if ~isempty(m)
+                                cell_j = m(1);
+                                I = day_to_linear(day_i, cell_i);
+                                J = day_to_linear(day_j, cell_j);
+                                A(I,J) = 1;
+                                A(J,I) = 1;
+                            end
+                        end
+                    end
+                end % day_j
+            end % day_i
+            
+            A = sparse(A); % Required by graphconncomp
+            [num_components, assignments] = graphconncomp(A, 'Directed', 'false');
+            
+            % Set up the match matrix; zero indicates unmatched
+            M = zeros(num_components, obj.num_days);
+            for I = 1:length(assignments)
+                assignment = assignments(I);
+                [k, cell_idx] = linear_to_day(I);
+                M(assignment, k) = cell_idx;
+            end
+            
+            % Helper indexing functions
+            function linear_idx = day_to_linear(day_idx, cell_idx)
+                day_idx_sparse = obj.full_to_sparse(day_idx);
+                linear_idx = offsets(day_idx_sparse) + cell_idx;
+            end % day_to_linear
+            
+            function [k, cell_idx] = linear_to_day(linear_idx)
+                k = find((linear_idx-offsets)>0, 1, 'last');
+                cell_idx = linear_idx - offsets(k);
+            end % linear_to_day
+        end
+        
         function M = compute_all_matches(obj)
             % Compute matching across all provided days. We keep only the
             % _classified cells_ of each day. Unmatched indices are
             % indicated by zeros.
             %------------------------------------------------------------
-            num_days = length(obj.valid_days);
-            
             base_day = obj.valid_days(1);
             base_day_cells = find(obj.ds{base_day}.is_cell);
             base_day_num_cells = length(base_day_cells);
             
             % Scratch space (sparse) for matching indices
-            M = zeros(base_day_num_cells, num_days);
+            M = zeros(base_day_num_cells, obj.num_days);
             M(:,1) = base_day_cells;
             
             % Fill out M columnwise in the order of obj.valid_days
-            for k = 2:num_days
+            for k = 2:obj.num_days
                 curr_day = obj.valid_days(k);
                 for x = 1:base_day_num_cells
                     for l = 1:(k-1)
