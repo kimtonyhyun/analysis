a611c7df7904093e2c67b3571cd457a59d67739a
inanhkn <inanh@stanford.edu>
2015-09-09 16:23:15 -0700

    add trial filtering

--- ds/@MultiDay/MultiDay.m
+++ ds/@MultiDay/MultiDay.m
@@ -119,26 +119,50 @@ classdef MultiDay < handle
             indices = obj.matched_indices(:, selected_days);
         end
         
-        function trials = get_trials(obj, day_idx, trial_indices,trial_portions)
-            if ~exist('trial_indices', 'var')
-                trial_indices = 1:obj.day(day_idx).num_trials;
-            end
-            trials = obj.day(day_idx).trials(trial_indices);
+        function trials = get_trials(obj, day_idx, varargin) 
             
-            if exist('trial_portions','var')
-                separate_trial_portions = 1;
-                selected_portions = [];
-                for i = 1:length(trial_portions)
-                    str = trial_portions{i};
-                    selected_portions(end+1) = find(strcmp({'pre','run','post'},str));
-                end    
+            % Defaults
+            num_trials = obj.day(day_idx).num_trials;
+            trial_indices = 1:num_trials;% get all trials
+            filtered_trials = true(num_trials,1);
+            separate_trial_portions = 0;
+            trial_portions = [];
+            
+            if ~isempty(varargin)
+                for k = 1:length(varargin)
+                    switch varargin{k}
+                        case isnumeric(varargin{k})
+                            trial_indices = varargin{k+1};
+                            if ~isvector(trial_indices) % not 1D
+                                error('trial_indices must be a vector.');
+                            end
+                        case {'start','end','turn'}
+                            var1 = varargin{k};
+                            var2 = varargin{k+1};
+                            filtered_trials = filtered_trials & ...
+                                obj.day(day_idx).filter_trials(var1,var2);
+                        case 'correct'
+                            filtered_trials = filtered_trials & ...
+                                obj.day(day_idx).filter_trials('correct');
+                        case 'incorrect'
+                        filtered_trials = filtered_trials & ...
+                            obj.day(day_idx).filter_trials('incorrect');
+                        case {'pre','run','post'}
+                            separate_trial_portions = 1;
+                            var1 = varargin{k};
+                            trial_portions(end+1) = find(strcmp({'pre','run','post'},var1));                            
+                    end
+                end
+            end
+            
+            trial_indices = intersect(trial_indices,find(filtered_trials));
+            trials = obj.day(day_idx).trials(trial_indices);
+
+            if separate_trial_portions
                 frame_indices = obj.day(day_idx).trial_indices(trial_indices,:);
                 % Remove offsets
                 frame_indices = bsxfun(@minus,frame_indices,frame_indices(:,1)-1);
-            else
-                separate_trial_portions = 0;
-            end
-
+            end            
             
             % Reorder the traces to match the common (matched) index
             day_cell_indices = obj.get_indices(day_idx);
@@ -150,7 +174,7 @@ classdef MultiDay < handle
                     idx_post = frame_indices(k,3)+1:frame_indices(k,4);
                     x = trials(k).traces;
                     x = {x(:,idx_pre),x(:,idx_run),x(:,idx_post)};
-                    trials(k).traces = x(selected_portions);
+                    trials(k).traces = x(trial_portions);
                 end
             end
         end
