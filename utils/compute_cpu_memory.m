986af17aed6fe6b54a6fc1b3772760e6101a84c5
inanhkn <inanh@stanford.edu>
2015-11-04 16:50:12 -0800

    add mac support for memory calculation

--- utils/compute_cpu_memory.m
+++ utils/compute_cpu_memory.m
@@ -2,10 +2,12 @@ function avail_mem = compute_cpu_memory()
 % Available memory on CPU in bytes
 
     if ispc % Windows
-        [~,sys] = memory;
-        avail_mem = (sys.PhysicalMemory.Available);
+         [~,sys] = memory;
+         avail_mem = (sys.PhysicalMemory.Available);
     elseif ismac %Mac
-        % TODO
+         [~,m]=unix('vm_stat | grep free');
+         spaces=strfind(m,' ');
+         avail_mem = str2double(m(spaces(end):end))*4096;
     else % Linux
          [~,meminfo] = system('cat /proc/meminfo');
          tokens = regexpi(meminfo,'^*MemAvailable:\s*(\d+)\s','tokens');
