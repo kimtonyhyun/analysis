324d53b09f9afa60108f83b2d89b2fb7a3a05c33
kimth <kimth@stanford.edu>
2015-11-09 12:26:36 -0800

    Factor out view_trial

--- view/view_detailed_raster.m
+++ view/view_detailed_raster.m
@@ -9,7 +9,7 @@ function view_detailed_raster(ds, cell_idx)
 %
 
 while (1)
-    scale = draw_rasters(); % Global normalized scaling for all trials
+    draw_rasters(); % Global normalized scaling for all trials
     
     % Ask user for command
     prompt = sprintf('Cell %d raster >> ', cell_idx);
@@ -20,12 +20,7 @@ while (1)
         if ~ds.is_behavior_loaded
             fprintf('  Behavior video not loaded into DaySummary!\n');
         else
-            while ~isnan(val) && ((1 <= val) && (val <= ds.num_trials))
-                draw_trial(val);
-                prompt = sprintf('  Showing trial %d >> ', val);
-                resp = strtrim(input(prompt, 's'));
-                val = str2double(resp);
-            end
+            view_detailed_trial(ds, cell_idx, val);
         end
     else
         resp = lower(resp);
@@ -46,7 +41,7 @@ end
 
     % Helper functions
     %------------------------------------------------------------
-    function raster_scale = draw_rasters()
+    function draw_rasters()
         clf;
         
         % Image of cell
@@ -93,89 +88,4 @@ end
         set(gca, 'CLim', raster_scale);
         title('North end'); 
     end % draw_rasters
-
-    function draw_trial(trial_idx)
-        clf;
-        
-        Mb = ds.get_behavior_trial(trial_idx); % Behavior movie
-        trace = ds.trials(trial_idx).traces(cell_idx,:);
-        num_frames_in_trial = length(trace);
-        
-        % Image of cell
-        %------------------------------------------------------------
-        subplot(3,4,[1 2]);
-        imagesc(ds.cells(cell_idx).im);
-        axis image;
-        title(sprintf('Cell %d (%s)', cell_idx, ds.cells(cell_idx).label));
-        colormap jet; freezeColors;
-        
-        % Zoom into the selected trial in the full raster
-        %------------------------------------------------------------
-        trial_window = 10;
-        trial_window_start = max(1, trial_idx-trial_window);
-        trial_window_end = min(ds.num_trials, trial_idx+trial_window);
-        
-        subplot(3,4,[5 6 9 10]);
-        ds.plot_cell_raster(cell_idx, 'draw_correct');
-        ylim([trial_window_start-0.5 trial_window_end+0.5]);
-        set(gca, 'YTick', trial_window_start:trial_window_end);
-        x_ends = get(gca, 'XLim');
-        line(x_ends, (trial_idx-0.5)*[1 1], 'Color', 'w', 'LineWidth', 2);
-        line(x_ends, (trial_idx+0.5)*[1 1], 'Color', 'w', 'LineWidth', 2);
-        
-        title('All trials');
-        colormap jet; freezeColors;
-        
-        % Show trace
-        %------------------------------------------------------------
-        trial_frame_indices = double(ds.trial_indices(trial_idx, :)); % [start open-gate close-gate end]
-        trial_frame_indices = trial_frame_indices - trial_frame_indices(1);
-        num_trials_in_frame = trial_frame_indices(4) + 1;
-        trial_markers = trial_frame_indices / num_trials_in_frame;
-        
-        subplot(3,4,[3 4]);
-        trial_phase = linspace(0, 1, num_frames_in_trial);
-        plot(trial_phase, trace, 'LineWidth', 2, 'HitTest', 'off');
-        xlim([0 1]);
-        ylim(scale);
-        grid on;
-        title(sprintf('Trial %d', trial_idx));
-        xlabel('Trial phase [a.u.]');
-        ylabel('Signal [a.u.]');
-        hold on;
-        
-        plot(trial_markers(2)*[1 1], scale, 'r--', 'HitTest', 'off'); % Open-gate
-        plot(trial_markers(3)*[1 1], scale, 'r--', 'HitTest', 'off'); % Close-gate
-        
-        % Markers for indicating current frame
-        t = plot(0*[1 1], scale); % Vertical bar
-        d = plot(0, trace(1), 'o', 'MarkerFaceColor', 'b', 'MarkerSize', 8); % Dot
-        set(gca, 'ButtonDownFcn', @update_frame);
-        
-        % Show behavior movie
-        %------------------------------------------------------------
-        subplot(3,4,[7 8 11 12]);
-        hb = imagesc(Mb(:,:,1));
-        set(gca, 'XTick', []);
-        set(gca, 'YTick', []);
-        axis image;
-        colormap gray;
-        title(sprintf('Frame 1 of %d', num_trials_in_frame));
-        
-        function update_frame(h, ~)
-            cp = get(h, 'CurrentPoint');
-            sel_phase = cp(1); % X point of click
-            sel_phase = max(sel_phase, 0);
-            sel_phase = min(sel_phase, 1);
-            sel_frame = 1 + round((num_frames_in_trial-1) * sel_phase);
-            
-            % Update visuals
-            set(t, 'XData', sel_phase*[1 1]);
-            set(d, 'XData', sel_phase, 'YData', trace(sel_frame));
-            set(hb, 'CData', Mb(:,:,sel_frame));
-            
-            subplot(3,4,[7 8 11 12]);
-            title(sprintf('Frame %d of %d', sel_frame, num_trials_in_frame));
-        end % update_frame
-    end % draw_trial
 end % view_cell_rasters
