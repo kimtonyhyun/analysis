d1a9bbab4875b3f9b207836555f1b692157ef825
kimth <kimth@stanford.edu>
2015-11-24 18:51:01 -0800

    More notes regarding definition of unmatched

--- ds/find_unmatched_cells.m
+++ ds/find_unmatched_cells.m
@@ -1,6 +1,14 @@
 function [unmatched_ids, matched_ids] = find_unmatched_cells(ds1, m_1to2)
 % Find classified cells in provided DaySummary ('ds1') with no match in
-% the corresponding matching ('m_1to2')
+% the corresponding matching ('m_1to2').
+%
+% NOTE: The result is dependent on _how_ the matching is computed. For 
+% example, if the matching is computed in a way that omits non-cells
+% (default behavior of `match_masks`) then cells in ds1 that overlap with
+% non-cells in ds2 are considered non-matched. If the matching is computed
+% including the non-cells, cells in ds1 are considered unmatched if they do
+% not match to any source (cell or non-cell) in ds2. Be careful!
+%
 
 is_cell = find(ds1.is_cell);
 
