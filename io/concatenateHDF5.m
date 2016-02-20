f6acf03b6206521a033532971ad7c5e867a1feb3
kimth <kimth@stanford.edu>
2016-02-19 17:37:51 -0800

    Simplified logic

--- io/concatenateHDF5.m
+++ io/concatenateHDF5.m
@@ -32,93 +32,71 @@ frameRate = str2double(xmlData.fps);
 
 % Initialize output HDF5 datset
 movie_dataset = '/Data/Images';
-chunkSize = [rows cols 1];
-h5create(fullfile(outputDir,hdf5Name),movie_dataset,[Inf Inf Inf],'ChunkSize',chunkSize,'Datatype','uint16');
+h5create(fullfile(outputDir,hdf5Name), movie_dataset, [Inf Inf Inf],...
+         'ChunkSize', [rows cols 1],...
+         'Datatype', 'uint16');
 
 % Indexing variables
-trialCount = 0;
-totalFrames = 0;
+trialCount = 0;  % Number of trials saved to HDF5
+totalFrames = 0; % Number of frames saved to HDF5
 
 startFrames = frame_indices(:,1);
-testFrames = startFrames(1,1);
+testFrame = 1;
 
 for i=1:num_files
-    if(i ~= 1)
-        %%% Find if the start frame of the next recording matches one of
-        %%% the start frame indicies
-        idx = find(testFrames == startFrames);
-        if(isempty(idx))
-            %%% Bad trial
-            badTrial = 1;
-            tif_info = imfinfo(fullfile(tifDir,list(i).name));
-            numBadFrames = length(tif_info);
-            testFrames = testFrames + numBadFrames;
-        else
-            %%% Good trial
-            badTrial = 0;
-        end
+    % Load recording
+    tifName = fullfile(tifDir,list(i).name);
+    imageStack = load_movie_from_tif(tifName);
+    
+    % Check XML for dropped frames
+    xmlName = convert_extension(tifName, 'xml');
+    xmlData = parse_miniscope_xml(xmlName);
+
+    numDroppedFrames = str2double(xmlData.dropped_count);
+    if(numDroppedFrames ~= 0)
+        droppedFrames = str2num(xmlData.dropped);
     else
-        if(testFrames ~= 1)
-            %%% Bad trial
-            badTrial = 1;
-            tif_info = imfinfo(fullfile(tifDir,list(i).name));
-            numBadFrames = length(tif_info);
-            testFrames = 1 + numBadFrames;
-        else
-            %%% Good trial
-            badTrial = 0;
-        end
+        droppedFrames = [];
+    end
+    
+    % Fill in dropped frames by its previous frame. Remarks:
+    %   1) Code will break if the first frame of file has been dropped.
+    %   2) Assumes that 'droppedFrames' is ascending.
+    for j=1:length(droppedFrames)
+        droppedFrame = droppedFrames(j);
+        fprintf('DroppedFrame: %i\n',droppedFrame);
+
+        frontStack = imageStack(:,:,1:droppedFrame-1);
+        backStack = imageStack(:,:,droppedFrame:end);
+        prev_frame = imageStack(:,:,droppedFrame-1);
+
+        imageStack = cat(3, frontStack, prev_frame, backStack);
+    end
+    clear frontStack backStack prev_frame;
+    
+    % Save frames to HDF5, if part of a good trial
+    %------------------------------------------------------------
+    if (testFrame == startFrames(1+trialCount)) % GOOD TRIAL        
+        trialCount = trialCount+1;
+
+        % Trim frames from the beginning and end
+        frames_to_save = imageStack(:,:,1+trim(1):end-trim(2));
+        numFrames = size(frames_to_save,3);
+        
+        h5write(fullfile(outputDir,hdf5Name), movie_dataset,...
+                frames_to_save,...
+                [1,1,1+totalFrames],...
+                size(frames_to_save));      
+        fprintf('%d: File "%s" stored\n', i, list(i).name);
+        
+        % Total frame count stored in hdf5 file
+        totalFrames = totalFrames+numFrames;
+    else % BAD TRIAL
+        fprintf('%d: File "%s" skipped\n', i, list(i).name);
     end
 
-    if (badTrial)
-        fprintf('  %d: File "%s" skipped\n', i, list(i).name);
-    else
-        % Add the good trial to the hdf5 file
-        trialCount = trialCount+1;
-        
-        % Read in the images
-        tifName = fullfile(tifDir,list(i).name);
-        imageStack = load_movie_from_tif(tifName);
-        oriFrames = size(imageStack, 3);
-        
-        % Check XML for dropped frames
-        xmlName = convert_extension(tifName, 'xml');
-        xmlData = parse_miniscope_xml(xmlName);
-        
-        numDroppedFrames = str2double(xmlData.dropped_count);
-        if(numDroppedFrames ~= 0)
-            droppedFrames = str2num(xmlData.dropped);
-        else
-            droppedFrames = [];
-        end
-        
-        %%% Replace any dropped frames with the frame immediately
-        %%% preceeding it
-        for j=1:length(droppedFrames)
-            fprintf('DroppedFrame: %i\n',droppedFrames(j));
-            droppedFrame = droppedFrames(j);
-            frontStack = imageStack(:,:,1:droppedFrame-1);
-            frontStack = cat(3,frontStack,imageStack(:,:,droppedFrame-1));
-            backStack = imageStack(:,:,droppedFrame:end);
-            imageStack = cat(3,frontStack,backStack);
-        end
-        
-        % Trim frames from the beginning and end
-        finalImageStack = imageStack(:,:,1+trim(1):end-trim(2));
-        numFrames = size(finalImageStack,3);
-        [dRows,dCols] = size(finalImageStack(:,:,1));
-        
-        h5write(fullfile(outputDir,hdf5Name),movie_dataset,finalImageStack,[1,1,1+totalFrames],[dRows,dCols,numFrames]);      
-        fprintf('  %d: File "%s" stored\n', i, list(i).name);
-        
-        %%% Total frame count stored in hdf5 file
-        totalFrames = totalFrames+numFrames;
-        
-        %%% Total frame count corresponding to behavior text file
-        testFrames = testFrames+oriFrames+numDroppedFrames;
-        
-        clear imageStack tifName tifInfo tifFile droppedFrames newImageStack
-    end
+    % Increment
+    testFrame = testFrame + size(imageStack,3);
 end
 
 h5create(fullfile(outputDir,hdf5Name),'/Params/TrimVals',[1 2],'Datatype','double');
