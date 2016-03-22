fa5b90bbb62ad54aa2b11deca030f5db11fe27b3
kimth <kimth@stanford.edu>
2016-03-22 12:14:16 -0700

    Apply common scaling to raster and spatial overlay

--- view/view_trace_in_space.m
+++ view/view_trace_in_space.m
@@ -3,15 +3,11 @@ function view_trace_in_space(ds, cell_idx)
 % behavior arena
 
 [m, M] = get_trace_min_max(ds, cell_idx);
+raster_scale = [m M];
 
 clf;
 
 subplot(2,4,1);
-% HACK: We are getting the common scale for the subsequent raster plots by
-% drawing a full raster, which will be "painted over".
-ds.plot_cell_raster(cell_idx, 'draw_correct');
-raster_scale = get(gca, 'CLim');
-
 ds.plot_cell_raster(cell_idx, 'start', 'west', 'draw_correct');
 set(gca, 'CLim', raster_scale);
 title('West start');
