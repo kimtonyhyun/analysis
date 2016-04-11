4659234851548e8c5af0adf4e6b3df47016f3c79
Fori <fori225@gmail.com>
2016-04-11 13:49:56 -0700

    Updating scripts to use .xy file instead of assuming centroids.mat

--- behavior/MouseTracking/FindTrials/mouse_path_plotter.m
+++ behavior/MouseTracking/FindTrials/mouse_path_plotter.m
@@ -1,4 +1,4 @@
-function [trial_frames] = mouse_path_plotter( movie, centroids_mat, varargin)
+function [trial_frames] = mouse_path_plotter( movie, centroids_xy, varargin)
 % Plots the path of the mouse during each trial based on position data and
 % trial frames (either specified by plusmaze txt or extracted from movie
 % using find_start_end_of_trials). Colormap = Jet --> Blue-ish = start of
@@ -6,7 +6,7 @@ function [trial_frames] = mouse_path_plotter( movie, centroids_mat, varargin)
 % 
 % Input:
 %     movie: Behavior video
-%     centroids_mat: position data from get_mouse_XY_pos
+%     centroids_xy: position data (.xy file) from get_mouse_XY_pos
 %     varargin: plusmaze txt file, e.g. 'mouse7_day09_allo-south.txt'
 % 
 % Output:
@@ -14,6 +14,7 @@ function [trial_frames] = mouse_path_plotter( movie, centroids_mat, varargin)
 %         by find_start_end_of_trials
 % 
 % 2015-03-01 Fori Wang
+% 2016-04-11 Updated Fori Wang
 
     % read in plusmaze source if available
     if ~isempty(varargin)
@@ -22,17 +23,19 @@ function [trial_frames] = mouse_path_plotter( movie, centroids_mat, varargin)
         trial_frames = [frame_indices(:,1) frame_indices(:,4)];
     else
         % if no plusmaze source, find trial start and ends from movie
-        [trial_frames,~]=find_start_end_of_trials(centroids_mat);
+        [trial_frames,~]=find_start_end_of_trials(centroids_xy);
     end
     
     % load centroids
-    load(centroids_mat)% assumes matrix saved as 'centroids'
+    centroids = load(centroids_xy);
     
     % read in behavior movie
     fprintf('Reading in Behavior Movie...\n');
     behavior_vid = VideoReader(movie);
-    
+    fprintf('Done!\n');
+    fprintf('Plotting mouse paths...\n');
     figure;
+    
     
     for trial_idx = 1:size(trial_frames,1)
         trial_start = trial_frames(trial_idx,1);
