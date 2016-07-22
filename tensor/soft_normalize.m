b8b4c6d94a257fef1b4018fe3bfd3c854508e680
alex williams <alex.h.willia@gmail.com>
2016-07-22 16:16:49 -0700

    add soft normalization

---
+++ tensor/soft_normalize.m
@@ -0,0 +1,15 @@
+function [Y] = soft_normalize(X)
+% SOFT_NORMALIZE, apply soft-normalization to each neuron in data tensor X and
+% return a new, normalized tensor Y.
+%
+%     [Y] = soft_normalize(X)
+
+Y = zeros(size(X));
+if ndims(X) == 3
+	for c = 1:size(X,1)
+		x = X(c,:,:);
+		Y(c,:,:) = x ./ (5 + range(x(:)));
+	end
+else
+	error('input has inappropriate dimensions')
+end
