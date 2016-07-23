7a07490d392ab4ca9e3babda7a70678756d52949
alex williams <alex.h.willia@gmail.com>
2016-07-22 20:12:20 -0700

    add plots and test

---
+++ tensor/test_tensor.m
@@ -0,0 +1,10 @@
+clear; close all; clc;
+
+cd('/home/alex/data/strategy_switch/c11m1d12')
+
+sources = gen_data_sources;
+session = DaySummary(sources, 'cm01');
+[X,cell_idx,trial_idx] = session_tensor(session);
+X = soft_normalize(X);
+cpd = fit_cpd(X);
+factor_plots(session,cpd(10,10),trial_idx);
