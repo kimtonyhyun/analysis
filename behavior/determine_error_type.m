6e6f8075c29e1eb883cd233b45ff958ebe33649e
Fori <forea@stanford.edu>
2016-09-01 16:26:24 -0700

    added function to compute perseverative errors

--- behavior/determine_error_type.m
+++ behavior/determine_error_type.m
@@ -1,24 +1,21 @@
-function [ num_perseverative_errors, num_random_errors ] = determine_error_type( file, strategy_switch, trial_start, trial_end)
+function [ num_persev_errors, num_random_errors ] = determine_error_type( file, strategy_switch, trial_start, trial_end)
 % Reads behavior txt file and given pre/post switch strategies,
 %       returns # of perseverative errors
 % inputs:
-%   strategy switch:
-%     - elan
-%     - elas
-%     - eran
-%     - eras
-%     - anel
-%     - asel
-%     - aner
-%     - aser
+%   strategy switch: elan, elas,eran,eras,anel,asel, aner, aser
 %   trial_start, trial_end: first_tria, last_trial, e.g. 56, 155
 %   file: plus maze text file
 %
 % parse plusmaze
 [~,maze_data,~] = parse_plusmaze(file);
+
+% consider only trials specified
+maze_data = maze_data(trial_start:trial_end,:);
+
     start_arm = maze_data(:,1);
     end_arm = maze_data(:,2);
     actual_end_arm = maze_data(:,3);
+    
 
 % for given strategy switch, designate switching arm (where persev errors
 % occur)
@@ -44,7 +41,10 @@ end
 incorr_trials = find(~strcmp(end_arm, actual_end_arm));
 
 % count how many incorrect trials have a start in perseverative path
-sum(strcmp(start_arm(incorr_trials),persev_start));
+num_persev_errors = sum(strcmp(start_arm(incorr_trials),persev_start));
+
+% assign all other errors as random errors
+num_random_errors = size(incorr_trials,1)-num_persev_errors;
 
 end
 
