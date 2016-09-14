4ae868ea283cba6fdef29b23dee0404d4ce9599e
Alex Williams <alex.h.willia@gmail.com>
2016-09-14 16:47:49 -0700

    normalize regress coef

--- tensor/visualize_cpd_regress.m
+++ tensor/visualize_cpd_regress.m
@@ -1,4 +1,4 @@
-function [B,cv_rate] = visualize_cpd_regress(cpd, meta)
+function [Bn,cv_rate] = visualize_cpd_regress(cpd, meta)
 % CPD_PREDICT(cpd, meta, trial_type)
 %
 % Uses logistic regression to predict trial metadata from cpd trial factors.
@@ -25,14 +25,18 @@ end
 % also regree on meta.strategy
 [Bs,cvs] = cpd_regress_strat(cpd, meta);
 
+% normalize regression coefficients
+Bn = [B Bs];
+Bn = Bn ./ repmat( sqrt(sum(Bn.^2)), size(Bn,1), 1);
+
 % make plot showing strength of regression coefficients
 figure()
-bar(0:15,abs([B Bs]))
+bar(0:15,abs(Bn))
 set(gca,'Xtick',0:15)
 legend({ sprintf('start location (%1.3f accuracy)',cv_rate(1)), ...
          sprintf('end location (%1.3f accuracy)',cv_rate(2)),...
          sprintf('correct choice (%1.3f accuracy)',cv_rate(3)),...
          sprintf('internal strategy (%1.3f accuracy)',cvs) });
 xlim([-1 16])
-ylabel('absolute value of regression coefficients')
+ylabel('abs val regression coefficients (normalized)')
 xlabel('trial factors (0 = intercept term)')
