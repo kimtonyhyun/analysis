0fff5c3a034a99b02d6aefed3f67827012647a75
Alex Williams <alex.h.willia@gmail.com>
2016-12-22 08:22:12 -0500

    simplify fit_cpd

--- tensor/fit_cpd.m
+++ tensor/fit_cpd.m
@@ -1,4 +1,4 @@
-function [cpd,rsq] = fit_cpd(X,varargin)
+function cpd = fit_cpd(X,varargin)
 % FIT_CPD, fits a series of cp decompositions of increasing rank and
 % plots the percentage of variance explained as function of model
 % complexity
@@ -11,7 +11,7 @@ p.addParameter('num_starts', 10);
 p.addParameter('min_rank', 15);
 p.addParameter('max_rank', 15);
 p.addParameter('printitn',true);
-p.addParameter('nonneg',false);
+p.addParameter('method','cp_als');
 p.parse(varargin{:});
 
 ns = p.Results.num_starts;
@@ -23,29 +23,26 @@ Xt = tensor(X);
 
 % create struct array
 nr = max_rank-min_rank+1;
-cpd(1,ns*nr) = struct('rank',0,'Rsq',0,'factors',struct([]),'lambdas',[],'decomp',[]);
+cpd(1,ns*nr) = struct('error',0,'decomp',[]);
 
 % main loop
-rsq = zeros(ns*nr,1);
-
 % iterate in random order for a better waitbar
 itercount = 0;
 for a = randperm(ns*nr)
     h = waitbar(itercount/(nr*ns));
     [r,~] = ind2sub([nr,ns],a);
     
-    if p.Results.nonneg
+    switch p.Results.method
+    case 'cp_nnals'
         decomp = normalize(cp_nnals(Xt,min_rank+r-1,'printitn',p.Results.printitn));
-    else
+    case 'cp_als'
         decomp = normalize(cp_als(Xt,min_rank+r-1,'printitn',p.Results.printitn));
+    case 'cprand'
+        decomp = normalize(cprand(Xt,min_rank+r-1,'printitn',p.Results.printitn));
     end
 
-	cpd(a).rank = min_rank+r-1;
-	cpd(a).Rsq = 1 - norm(Xt-full(decomp))/norm(Xt - mean(X(:)));
-	cpd(a).factors = struct('neuron',decomp.u{1},'time',decomp.u{2},'trial',decomp.u{3});
-	cpd(a).lambda = decomp.lambda;
-    cpd(a).decomp = decomp;
-    rsq(a) = cpd(a).Rsq;
+	cpd(a).error = norm(Xt-full(decomp))/norm(Xt - mean(X(:)));
+	cpd(a).decomp = decomp;
     itercount = itercount+1;
 end
 close(h)
