3f93629b2bd1d499d9979f3cade98bb138503541
kimth <kimth@stanford.edu>
2017-01-04 16:46:35 -0800

    Also add peak-to-peak measure

--- evaluator/compute_all_scores.m
+++ evaluator/compute_all_scores.m
@@ -7,7 +7,7 @@ score = struct('name', '',...       % String name of score
                'vals', [],...      % Scores: [num_neurons x num_trials]
                'vals_n', []);      % Score per neuron: [num_neurons x 1]
 
-num_score_types = 2;
+num_score_types = 3;
 scores = repmat(score, num_score_types, 1);
             
 scores(1).name = 'Rsq';
@@ -17,3 +17,10 @@ scores(1).sort_order = 'descend';
 scores(2).name = 'Residual';
 [scores(2).vals, scores(2).vals_n] = compute_residuals(X, Xest);
 scores(2).sort_order = 'ascend';
+
+scores(3).name = 'Peak-to-peak';
+Xmin = squeeze(min(X,[],2));
+Xmax = squeeze(max(X,[],2));
+scores(3).vals = Xmax - Xmin;
+scores(3).vals_n = max(Xmax,[],2) - min(Xmin,[],2);
+scores(3).sort_order = 'descend';
