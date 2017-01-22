7ed96805e4cab8d5cfe15b6216f76bc2b836f31b
kimth <kimth@stanford.edu>
2017-01-22 10:03:32 -0800

    Support unfoldings along different dimensions

--- eval_fit/fit_nnmf.m
+++ eval_fit/fit_nnmf.m
@@ -1,27 +1,50 @@
-function [N, T, Xest] = fit_nnmf(X, r)
-% Fit NNMF model of rank r by unwrapping data tensor X.
+function [A, B, Xest] = fit_nnmf(X, unfold_dim, r)
+% Fit NNMF model of rank r by unfolding the data tensor X along a specified
+% dimension.
 %
 % Output format:
-%   N(:,i) = Neuron loadings of the i-th factor
-%   T(:,j,i) = Temporal loading of the i-th factor, j-th trial
-%   Xest: Reconstructed tensor, same dimension as the data tensor X
+%   - A(:,i) = Loadings on the unfolded dimension of the i-th factor
+%   - B(:,j,i) =
+%       For neural unfolding: Samples of the i-th factor, j-th TRIAL
+%       For trial unfolding: Samples of the i-th factor, j-th NEURON
+%   - Xest: Reconstructed tensor, same dimension as the data tensor X
+%
+% Usage examples:
+%   - Unfold on neurons: [N, TrialTime, Xest] = fit_nnmf(X, 1, r);
+%   - Unfold on trials: [T, NeuronTime, Xest] = fit_nnmf(X, 3, r);
 %
 
+% Canonical layout of data tensor X is [neurons x time x trials]
 [num_neurons, num_samples, num_trials] = size(X);
-Xu = reshape(X, [num_neurons, num_samples*num_trials]); % Unwrap along time
 
+switch unfold_dim
+    case 1 % Neuron unfolding. (Fast-axis=Samples; Slow-axis=Trials)
+        fprintf('Applying NEURAL unfolding; Xu=[num_neurons x num_samples(fast)*num_trials(slow)]\n');
+        unfold_p = [1 2 3];
+    case 3 % Trial unfolding. (Fast-axis=Samples, Slow-axis=Neurons)
+        fprintf('Applying TRIAL unfolding; Xu=[num_trials x num_samples(fast)*num_neurons(slow)]\n');
+        unfold_p = [3 2 1];
+    otherwise
+        error('fit_nnmf: Cannot unfold X along dim=%d!', unfold_dim);
+end
+
+% Generate the unfolded matrix along the requested direction
+Xu = permute(X, unfold_p);
+[~, num_fast, num_slow] = size(Xu);
+Xu = reshape(Xu, [size(Xu,1), num_fast*num_slow]);
+
+% Perform NMF on the unfolded matrix
 options = statset('nnmf');
 options.Display = 'iter'; % Verbose output
 options.MaxIter = 200;
-
-% 'nnmf' output format:
-%   N(:,i) = Neuron loadings of the i-th factor
-%   T(i,:) = Temporal (unwrapped) loading of the i-th factor
-[N,T] = nnmf(Xu, r, 'options', options,...
+[A,B] = nnmf(Xu, r, 'options', options,...
              'algorithm', 'als');
                 
-Xest = N*T; % Reconstructed tensor
-Xest = reshape(Xest, [num_neurons, num_samples, num_trials]); % Rewrap into tensor
+Xest = A*B; % Reconstructed tensor
 
-T = reshape(T, [r, num_samples, num_trials]);
-T = permute(T, [2 3 1]); % [Intra-trial time x trials x rank]
+% Invert the permutation of dimensions of X back into canonical form
+Xest = ipermute(Xest, unfold_p);
+Xest = reshape(Xest, [num_neurons, num_samples, num_trials]);
+
+B = reshape(B, [r, num_fast, num_slow]);
+B = permute(B, [2 3 1]); % [Fast-dim x Slow-dim x rank]
