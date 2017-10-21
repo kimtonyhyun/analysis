91363054b5b88faa35dcb980dbe6da09455caecb
Tony <kimth@stanford.edu>
2017-10-21 14:27:12 -0700

    Display the mean+sem traces

--- ds/@DaySummary/plot_cell_raster.m
+++ ds/@DaySummary/plot_cell_raster.m
@@ -1,4 +1,4 @@
-function [raster, trial_inds] = plot_cell_raster(obj, cell_idx, varargin)
+function [raster, aligned_time, trial_inds] = plot_cell_raster(obj, cell_idx, varargin)
 % Plots a raster of cell activity, where trials are aligned to the closing
 % gate frame.
 %
@@ -68,7 +68,8 @@ function [raster, trial_inds] = plot_cell_raster(obj, cell_idx, varargin)
         end
     end
 
-    imagesc(pre_offset:post_offset, 1:num_filtered_trials, raster, 'HitTest', 'off');
+    aligned_time = pre_offset:post_offset;
+    imagesc(aligned_time, 1:num_filtered_trials, raster, 'HitTest', 'off');
     colormap jet;
     xlabel(align_str);
     ylabel('Trial index');
--- view/touch/draw_path_subrasters.m
+++ view/touch/draw_path_subrasters.m
@@ -2,55 +2,68 @@ function draw_path_subrasters(ds, cell_idx, raster_scale)
 % Used by 'view_raster_touch'
 
 sd = ds.switchdata;
+trials = ds.get_switch_trials;
+
+constant_path_title = sprintf('Constant path (%s)', sd.constant_path_start);
+changing_path_title = sprintf('Changing path (%s)', sd.changing_path_start);
 
 % First column is for CONSTANT path
-subplot(2,4,3);
-[~, trial_inds] = ds.plot_cell_raster(cell_idx,...
-    'range', sd.pre_switch_trials,...
-    'start', sd.constant_path_start,...
-    'correct', 'draw_correct');
-set(gca, 'CLim', raster_scale);
-title(sprintf('Constant path (%s-start)', sd.constant_path_start));
-n_trials = length(trial_inds);
-ylabel(sprintf('PRE-switch (%d)', n_trials));
-yticks(1:n_trials);
-yticklabels(num2cell(trial_inds));
+%------------------------------------------------------------
+subplot(5,4,[3 7]);
+% Note that 'aligned_time' is common to all traces
+[constant_pre_traces, aligned_time] = draw_subraster(trials.constant_pre, 'PRE-switch');
+title(constant_path_title);
 
-subplot(2,4,7);
-[~, trial_inds] = ds.plot_cell_raster(cell_idx,...
-    'range', sd.post_switch_trials,...
-    'start', sd.constant_path_start,...
-    'correct', 'draw_correct');
-set(gca, 'CLim', raster_scale);
-title(sprintf('Constant path (%s-start)', sd.constant_path_start));
-n_trials = length(trial_inds);
-ylabel(sprintf('POST-switch (%d)', n_trials));
-yticks(1:n_trials);
-yticklabels(num2cell(trial_inds));
+subplot(5,4,[11 15]);
+constant_post_traces = draw_subraster(trials.constant_post, 'POST-switch');
+title(constant_path_title);
+
+subplot(5,4,19);
+draw_pre_post_averages(constant_pre_traces, constant_post_traces);
+title(constant_path_title);
 
 % Second column is for CHANGING path
-subplot(2,4,4);
-[~, trial_inds] = ds.plot_cell_raster(cell_idx,...
-    'range', sd.pre_switch_trials,...
-    'start', sd.changing_path_start,...
-    'correct', 'draw_correct');
-set(gca, 'CLim', raster_scale);
-title(sprintf('Changing path (%s-start)', sd.changing_path_start));
-n_trials = length(trial_inds);
-ylabel(sprintf('PRE-switch (%d)', n_trials));
-yticks(1:n_trials);
-yticklabels(num2cell(trial_inds));
+%------------------------------------------------------------
+subplot(5,4,[4 8]);
+changing_pre_traces = draw_subraster(trials.changing_pre, 'PRE-switch');
+title(changing_path_title);
 
-subplot(2,4,8);
-[~, trial_inds] = ds.plot_cell_raster(cell_idx,...
-    'range', sd.post_switch_trials,...
-    'start', sd.changing_path_start,...
-    'correct', 'draw_correct');
-set(gca, 'CLim', raster_scale);
-title(sprintf('Changing path (%s-start)', sd.changing_path_start));
-n_trials = length(trial_inds);
-ylabel(sprintf('POST-switch (%d)', n_trials));
-yticks(1:n_trials);
-yticklabels(num2cell(trial_inds));
+subplot(5,4,[12 16]);
+changing_post_traces = draw_subraster(trials.changing_post, 'POST-switch');
+title(changing_path_title);
+
+subplot(5,4,20);
+draw_pre_post_averages(changing_pre_traces, changing_post_traces);
+title(changing_path_title);
+
+    function [raster, aligned_time] = draw_subraster(trial_inds, y_label)
+        % Wrapper around 'plot_cell_raster' to apply some common formatting
+        % for the current context
+        [raster, aligned_time] = ds.plot_cell_raster(cell_idx, 'inds', trial_inds);
+        set(gca, 'CLim', raster_scale);
+        N = length(trial_inds);
+        yticks(1:N);
+        yticklabels(num2cell(trial_inds));
+        ylabel(sprintf('%s (%d)', y_label, N));
+        xlabel('');
+    end % draw_subraster
+
+    function draw_pre_post_averages(pre_traces, post_traces)
+        % Compute the mean and the SEM
+        pre_mean = mean(pre_traces,1);
+        pre_err = std(pre_traces,[],1)/sqrt(size(pre_traces,1));
+        post_mean = mean(post_traces,1);
+        post_err = std(post_traces,[],1)/sqrt(size(post_traces,1));
+        
+        shadedErrorBar(aligned_time, pre_mean, pre_err, 'b');
+        hold on;
+        shadedErrorBar(aligned_time, post_mean, post_err, 'r');
+        hold off;
+        xlim(aligned_time([1 end]));
+        ylim(raster_scale);
+        grid on;
+        xlabel('Frames relative to gate close');
+        ylabel('Fluorescence');
+    end % draw_pre_post_averages
 
 end % draw_path_subrasters
