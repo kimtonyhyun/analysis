fd45bf7fb930fd4f5e9a5656442519cd4cbe834c
kimth <kimth@stanford.edu>
2017-10-25 15:48:10 -0700

    Move event support functions

---
+++ signal_op/compute_trace_stats.m
@@ -0,0 +1,15 @@
+function stats = compute_trace_stats(trace)
+
+stats.num_bins = 500; % FIXME: Set dynamically
+[n, x] = hist(trace, stats.num_bins);
+
+nonzero_inds = n>0;
+stats.hist_counts = n(nonzero_inds);
+stats.hist_centers = x(nonzero_inds);
+
+[~, max_ind] = max(n);
+stats.mode = x(max_ind);
+
+% Also compute percentiles
+ps = [95 96 97 98 99]';
+stats.percentiles = [ps prctile(trace,ps)];
