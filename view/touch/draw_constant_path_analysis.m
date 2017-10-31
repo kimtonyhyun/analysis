a4aa633f66fd47f67b52ccf3b553333f83bba393
kimth <kimth@stanford.edu>
2017-10-31 15:40:07 -0700

    Some cleanup

--- view/touch/draw_constant_path_analysis.m
+++ view/touch/draw_constant_path_analysis.m
@@ -6,50 +6,64 @@ trials = ds.get_switch_trials;
 pre_trials = trials.constant_pre;
 post_trials = trials.constant_post;
 
+x_pre = 1:length(pre_trials);
+x_post = x_pre(end) + (1:length(post_trials));
+x_range = [1 x_post(end)];
+
 pre = compute_features(ds, cell_idx, pre_trials);
 post = compute_features(ds, cell_idx, post_trials);
 
 
 % Behavioral trial times
 subplot(4,2,2);
-stem(pre_trials, pre.times, 'b');
+stem(x_pre, pre.times, 'b');
 hold on;
-stem(post_trials, post.times, 'r');
+stem(x_post, post.times, 'r');
 hold off;
-% xlabel('Trial index');
-xlim([1 ds.num_trials]);
+xlim(x_range);
 ylabel('Trial times (s)');
+ylim(compute_ylim(pre.times, post.times));
 grid on;
 title('Constant path: Pre (blue) vs. Post (red)');
 
 % Fluorescence averages
 subplot(4,2,4);
-stem(pre_trials, pre.mean_fluorescence, 'b');
+stem(x_pre, pre.mean_fluorescence, 'b');
 hold on;
-stem(post_trials, post.mean_fluorescence, 'r');
+stem(x_post, post.mean_fluorescence, 'r');
 hold off;
-% xlabel('Trial index');
-xlim([1 ds.num_trials]);
-grid on;
+xlim(x_range);
 ylabel('Avg fluorescence');
+ylim(compute_ylim(pre.mean_fluorescence, post.mean_fluorescence));
+grid on;
 
 % Event amplitude sum
 subplot(4,2,6);
-stem(pre_trials, pre.event_sum, 'b');
+stem(x_pre, pre.event_sum, 'b');
 hold on;
-stem(post_trials, post.event_sum, 'r');
+stem(x_post, post.event_sum, 'r');
 hold off;
-% xlabel('Trial index');
-xlim([1 ds.num_trials]);
+xlim(x_range);
 ylabel('\Sigma Event amplitudes');
+ylim(compute_ylim(pre.event_sum, post.event_sum));
 grid on;
 
 subplot(4,2,8);
-stem(pre_trials, pre.num_events, 'b');
+stem(x_pre, pre.num_events, 'b');
 hold on;
-stem(post_trials, post.num_events, 'r');
+stem(x_post, post.num_events, 'r');
 hold off;
 xlabel('Trial index');
-xlim([1 ds.num_trials]);
+xlim(x_range);
 ylabel('Event counts');
+ylim(compute_ylim(pre.num_events, post.num_events));
 grid on;
+
+end % draw_constant_path_analysis
+
+function y_range = compute_ylim(pre_vals, post_vals)
+    all_vals = [pre_vals(:); post_vals(:)];
+    m = min(min(all_vals), 0);
+    M = max(all_vals);
+    y_range = [m M] + 0.1*(M-m)*[-1 1];
+end
