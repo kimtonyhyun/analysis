2f6f74d50e5a828187e4470d5827170d1b4e2427
kimth <kimth@stanford.edu>
2017-10-31 15:49:28 -0700

    More refactor

--- view/touch/draw_constant_path_analysis.m
+++ view/touch/draw_constant_path_analysis.m
@@ -13,51 +13,36 @@ x_range = [1 x_post(end)];
 pre = compute_features(ds, cell_idx, pre_trials);
 post = compute_features(ds, cell_idx, post_trials);
 
-
 % Behavioral trial times
 subplot(4,2,2);
-stem(x_pre, pre.times, 'b');
-hold on;
-stem(x_post, post.times, 'r');
-hold off;
-xlim(x_range);
+draw_stem(pre.times, post.times);
 ylabel('Trial times (s)');
-ylim(compute_ylim(pre.times, post.times));
-grid on;
 title('Constant path: Pre (blue) vs. Post (red)');
 
 % Fluorescence averages
 subplot(4,2,4);
-stem(x_pre, pre.mean_fluorescence, 'b');
-hold on;
-stem(x_post, post.mean_fluorescence, 'r');
-hold off;
-xlim(x_range);
+draw_stem(pre.mean_fluorescence, post.mean_fluorescence);
 ylabel('Avg fluorescence');
-ylim(compute_ylim(pre.mean_fluorescence, post.mean_fluorescence));
-grid on;
 
 % Event amplitude sum
 subplot(4,2,6);
-stem(x_pre, pre.event_sum, 'b');
-hold on;
-stem(x_post, post.event_sum, 'r');
-hold off;
-xlim(x_range);
+draw_stem(pre.event_sum, post.event_sum);
 ylabel('\Sigma Event amplitudes');
-ylim(compute_ylim(pre.event_sum, post.event_sum));
-grid on;
 
 subplot(4,2,8);
-stem(x_pre, pre.num_events, 'b');
-hold on;
-stem(x_post, post.num_events, 'r');
-hold off;
+draw_stem(pre.num_events, post.num_events);
 xlabel('Trial index');
-xlim(x_range);
 ylabel('Event counts');
-ylim(compute_ylim(pre.num_events, post.num_events));
-grid on;
+
+    function draw_stem(pre_vals, post_vals)
+        stem(x_pre, pre_vals, 'b', 'HitTest', 'off');
+        hold on;
+        stem(x_post, post_vals, 'r', 'HitTest', 'off');
+        hold off;
+        xlim(x_range);
+        ylim(compute_ylim(pre_vals, post_vals));
+        grid on;
+    end % draw_stem
 
 end % draw_constant_path_analysis
 
@@ -65,5 +50,10 @@ function y_range = compute_ylim(pre_vals, post_vals)
     all_vals = [pre_vals(:); post_vals(:)];
     m = min(min(all_vals), 0);
     M = max(all_vals);
-    y_range = [m M] + 0.1*(M-m)*[-1 1];
+    
+    if (m ~= M)
+        y_range = [m M] + 0.1*(M-m)*[-1 1];
+    else
+        y_range = m + [-0.5 0.5];
+    end
 end
