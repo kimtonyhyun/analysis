e9836bc8e4a96584f52a20db1475cf919357ae15
kimth <kimth@stanford.edu>
2018-02-05 08:55:22 -0800

    Rename and tweak merge md

--- ds/create_merge_md.m
+++ ds/create_merge_md.m
@@ -1,15 +1,22 @@
-function md = create_extraction_md(ds_array)
+function md = create_merge_md(ds_array)
 % Helper function to create MultiDay instance to represent multiple
-% extraction runs on the same dataset.
+% extraction runs on the same dataset, to be merged into one set. Used, for
+% example, with iterative CNMF or creating union filter sets across days.
 %
 % Usage:
-%   md = create_extraction_md([ds1 ds2 ds3]);
+%   md = create_merge_md([ds1 ds2 ds3]);
 
 num_ds = length(ds_array);
 ds_list = cell(num_ds, 2);
 for k = 1:num_ds
+    ds = ds_array(k);
+    if all(cellfun(@isempty, ds.get_class))
+        fprintf('create_merge_md: DS%d is completely unlabeled. Setting all sources to be cells!\n', k);
+        ds.set_labels;
+    end
+    
     ds_list{k,1} = k;
-    ds_list{k,2} = ds_array(k);
+    ds_list{k,2} = ds;
 end
 
 match_list = cell(num_ds-1, 4);
