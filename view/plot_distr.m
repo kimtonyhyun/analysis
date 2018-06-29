83772c491c52144b24be9e0b6cd02ba3cb08b760
Tony <kimth@stanford.edu>
2018-06-28 20:38:18 -0700

    Play with 10-90

--- view/plot_distr.m
+++ view/plot_distr.m
@@ -2,17 +2,20 @@ function [X, Y] = plot_distr(ys, color, x_offset)
 
 num_groups = length(ys);
 
-% Compute the 25th, 50th (median), 75th percentiles of each group
-quartiles = cellfun(@(y) prctile(y,[25 50 75]), ys, 'UniformOutput', false);
+% Compute the desired percentiles of each group
+quartiles = cellfun(@(y) prctile(y,[10 25 50 75 90]), ys, 'UniformOutput', false);
 quartiles = cell2mat(quartiles);
 
 % Plot the 25-75 range
 x = 1:num_groups;
 X = kron(x, [1 1 NaN]);
-Y = [quartiles(:,[1 3]) NaN(num_groups,1)]';
-Y = Y(:);
-plot(X + x_offset, Y, 'Color', color);
+Y = [quartiles(:,[2 4]) NaN(num_groups,1)]';
+plot(X + x_offset, Y(:), 'LineWidth', 1, 'Color', color);
 hold on;
 
+% % Plot the 10-90 range
+% Y = [quartiles(:,[1 5]) NaN(num_groups,1)]';
+% plot(X + x_offset, Y(:), 'LineWidth', 1, 'Color', color);
+
 % Plot the median
-plot(x + x_offset, quartiles(:,2), '.', 'Color', color);
+plot(x + x_offset, quartiles(:,3), '.', 'MarkerSize', 16, 'Color', color);
