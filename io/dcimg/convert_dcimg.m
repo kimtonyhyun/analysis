d856962e951d33c83cfb097f555b85b90f807fa0
kimth <kimth@stanford.edu>
2020-09-12 23:38:28 -0700

    Tweaks to DCIMG converter

--- io/dcimg/convert_dcimg.m
+++ io/dcimg/convert_dcimg.m
@@ -1,7 +1,13 @@
-function outname = convert_dcimg(source)
+function movie_out = convert_dcimg(movie_in, movie_out)
 % Convert Hamamatsu DCIMG into a HDF5 file. TODO: Is converting one frame
 % at a time truly the only way to access DCIMG data?
 %
+
+if ~exist('movie_out', 'var')
+    [~, movie_out] = fileparts(movie_in);
+    movie_out = sprintf('%s.hdf5', movie_out);
+end
+fprintf('convert_dcimg: Output movie will be saved as "%s"\n', movie_out);
 
 % Analyze DCIMG file
 %------------------------------------------------------------
@@ -15,12 +21,9 @@ num_frames = double(num_frames);
 
 % Prep output
 %------------------------------------------------------------
-[~, outname] = fileparts(source);
-outname = sprintf('%s.hdf5', outname);
 dataset_name = '/Data/Images';
 
-fprintf('convert_dcimg: Output movie will be saved as "%s"\n', outname);
-h5create(outname, dataset_name, [height width num_frames],...
+h5create(movie_out, dataset_name, [height width num_frames],...
     'DataType', data_type,...
     'ChunkSize', [height width 1]);
 
@@ -33,12 +36,12 @@ for k = 1:num_frames
     end
     
     A = dcimgmatlab(int32(k-1), source); % 0-based indexing
-    h5write(outname, dataset_name, A, [1 1 k], [height width 1]);
+    h5write(movie_out, dataset_name, A, [1 1 k], [height width 1]);
 end
 
 % Create standard "/Params" directory
-h5create(outname, '/Params/NumFrames', 1);
-h5write(outname, '/Params/NumFrames', num_frames);
+h5create(movie_out, '/Params/NumFrames', 1);
+h5write(movie_out, '/Params/NumFrames', num_frames);
 
 clear mex; % Final cleanup
 fprintf('%s: Done!\n', datestr(now));
