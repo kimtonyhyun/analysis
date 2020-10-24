30f7d30904b16cea4d9fe4183cce5a9f2a18abc3
kimth <kimth@stanford.edu>
2020-10-24 10:20:10 -0700

    Simplify boxplot_wrapper call

--- utils/boxplot_wrapper.m
+++ utils/boxplot_wrapper.m
@@ -1,24 +1,31 @@
-function [d, g] = boxplot_wrapper(grouped_data, varargin)
+function [d, g] = boxplot_wrapper(group_names, group_data, varargin)
 % A wrapper for 'boxplot' that allows a cell-based interface, where:
-%   - grouped_data: [num_groups x 2] cell, where
-%       - grouped_data{k,1}: Name of group. All numeric or all string.
-%       - grouped_data{k,2}: Data belonging to group. Needs to be column
-%       vector
+%   - group_names: Names of groups. All numeric or all string.
+%   - group_data: Data belonging to each group.
 
-num_groups = size(grouped_data, 1);
+group_names = group_names(:); % Force column vector
+
+num_groups = size(group_names, 1);
 num_data_per_group = zeros(num_groups, 1);
 
 for k = 1:num_groups
-    num_data_per_group(k) = length(grouped_data{k,2});
+    gdk = group_data{k};
+    num_data_per_group(k) = length(gdk);
+    
+    group_data{k} = gdk(:); % Force column vector
 end
 num_all_data = sum(num_data_per_group);
 
 % Interface to boxplot
-d = cell2mat(grouped_data(:,2));
+d = cell2mat(group_data);
 g = cell(num_all_data, 1);
 idx = 1;
 for k = 1:num_groups
-    g(idx:idx+num_data_per_group(k)-1) = grouped_data(k,1);
+    if iscell(group_names)
+        g(idx:idx+num_data_per_group(k)-1) = group_names(k);
+    else
+        g(idx:idx+num_data_per_group(k)-1) = {group_names(k)};
+    end
     idx = idx + num_data_per_group(k);
 end
 
