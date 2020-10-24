510777f9723581022d647b80030b51b3eda7d445
kimth <kimth@stanford.edu>
2020-10-24 14:06:40 -0700

    Flesh out utils

--- utils/boxplot_wrapper.m
+++ utils/boxplot_wrapper.m
@@ -2,8 +2,12 @@ function [d, g] = boxplot_wrapper(group_names, group_data, varargin)
 % A wrapper for 'boxplot' that allows a cell-based interface, where:
 %   - group_names: Names of groups. All numeric or all string.
 %   - group_data: Data belonging to each group.
+%
+% Note that 'boxplot' will only sort the groups in increasing group_name
+% if the names are numeric!
 
 group_names = group_names(:); % Force column vector
+group_data = group_data(:);
 
 num_groups = size(group_names, 1);
 num_data_per_group = zeros(num_groups, 1);
@@ -12,7 +16,9 @@ for k = 1:num_groups
     gdk = group_data{k};
     num_data_per_group(k) = length(gdk);
     
-    group_data{k} = gdk(:); % Force column vector
+    % Force column vector. Note that both the containing cell and its 
+    % contents are column vectors. Needed for 'cell2mat', below.
+    group_data{k} = gdk(:);
 end
 num_all_data = sum(num_data_per_group);
 
--- utils/tight_plot.m
+++ utils/tight_plot.m
@@ -1,6 +1,6 @@
-function y_lims = tight_plot(x, y)
+function y_lims = tight_plot(x, y, varargin)
 % Wrapper around "plot(x,y)" that automatically sizes xlim and ylim.
-plot(x, y);
+plot(x, y, varargin{:});
 xlim(x([1 end]));
 y_lims = [min(y) max(y)];
 y_lims = y_lims + 0.1*diff(y_lims)*[-1 1];
