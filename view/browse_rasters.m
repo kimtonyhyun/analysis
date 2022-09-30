9e339b52326a014adbd60e5ed683387a3a6946d9
kimth <kimth@stanford.edu>
2022-09-29 21:56:57 -0700

    Re-add title

--- view/browse_rasters.m
+++ view/browse_rasters.m
@@ -102,7 +102,7 @@ end
 %             d1 = compute_trial_raster_dimensionality(R, 'dim');
 %             d2 = compute_trial_raster_dimensionality(R, 'erank');
 %             title(sprintf('Cell %d\ndim=%.3f; erank=%.1f%%', cell_idx, d1, 100*d2));
-%             title(sprintf('Cell %d', cell_idx));
+            title(sprintf('Cell %d', cell_idx));
         end
         
         function cells_on_page = get_cells_on_page(page_idx)
