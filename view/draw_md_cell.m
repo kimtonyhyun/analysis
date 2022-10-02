4f5a33f767380b2629757568a19f31ec3e274a13
kimth <kimth@stanford.edu>
2022-10-02 13:29:59 -0700

    Provision for multiple rasters per cell

--- view/draw_md_cell.m
+++ view/draw_md_cell.m
@@ -1,7 +1,25 @@
 function draw_md_cell(md, common_cell_idx, raster_fn)
 
+% Original behavior: Use the built-in raster visualization function of DS
 if ~exist('raster_fn', 'var')
     raster_fn = @(ds,k) plot_cell_raster(ds, k, 'draw_correct');
+end
+
+% We allow 'raster_fn' to be a cell array of multiple raster visualization
+% functions. The following allows for a common call interface, whether 1 or
+% more raster_fns are provided.
+%
+% Each 'raster_fn' should be a function handle with the arguments @(ds,k)
+% where ds is the DaySummary and k is the cell index.
+if length(raster_fn) == 1
+    raster_fn = {raster_fn};
+end
+num_rasters = length(raster_fn);
+
+if num_rasters > 1
+    sp_num_rows = 1 + 2*num_rasters;
+else % num_rasters == 1
+    sp_num_rows = 4;
 end
 
 sp = @(m,n,p) subtightplot(m, n, p, 0.05, 0.05, 0.05); % Gap, Margin-X, Margin-Y
@@ -13,7 +31,7 @@ for k = 1:md.num_days
     ds = md.day(day); % Just a shorthand
 
     % Draw image of cell on each day
-    sp(4, md.num_days, k);
+    sp(sp_num_rows, md.num_days, k);
     cell_image_k = ds.cells(cell_idx_k).im;
     cell_com_k = ds.cells(cell_idx_k).com;
     zoom_half_width = min(size(cell_image_k))/10;
@@ -31,34 +49,42 @@ for k = 1:md.num_days
     end
 
     % Draw raster
-    sp(4, md.num_days, [md.num_days+k 2*md.num_days+k]);
-    raster_fn(ds, cell_idx_k);
+    for r = 1:num_rasters
+        sp(sp_num_rows, md.num_days,...
+            [(1+2*(r-1))*md.num_days+k (2+2*(r-1))*md.num_days+k]);
+        raster_fn{r}(ds, cell_idx_k);
+    end
 
     % Get trace
     traces{k} = ds.get_trace(cell_idx_k);
 end
 
-% Draw traces on a single plot
-trace_offsets = zeros(1+md.num_days, 1);
-for k = 1:md.num_days
-    trace_offsets(k+1) = length(traces{k});
+% If we're plotting only 1 raster per cell, then plot cross-day traces as a
+% single plot in the bottom row. If there are more than 1 raster per cell,
+% we skip this in order to conserve vertical space
+if num_rasters == 1
+    trace_offsets = zeros(1+md.num_days, 1);
+    for k = 1:md.num_days
+        trace_offsets(k+1) = length(traces{k});
+    end
+    trace_offsets = cumsum(trace_offsets);
+    
+    colors = 'rbk';
+    sp(sp_num_rows, md.num_days,...
+        ((sp_num_rows-1)*md.num_days+1):(sp_num_rows*md.num_days));
+    for k = 1:md.num_days
+        color = colors(1+mod(k,length(colors)));
+        plot(trace_offsets(k):(trace_offsets(k+1)-1),...
+             traces{k}, color);
+        hold on;
+    end
+    hold off;
+    grid on;
+    xlim([0 trace_offsets(end)-1]);
+    set(gca, 'XTick', []);
+    full_trace = cell2mat(traces);
+    M = max(full_trace);
+    m = min(full_trace);
+    ylim([m M] + 0.1*(M-m)*[-1 1]);
+    ylabel('Trace [a.u.]');
 end
-trace_offsets = cumsum(trace_offsets);
-
-colors = 'rbk';
-sp(4, md.num_days, (3*md.num_days+1):(4*md.num_days));
-for k = 1:md.num_days
-    color = colors(1+mod(k,length(colors)));
-    plot(trace_offsets(k):(trace_offsets(k+1)-1),...
-         traces{k}, color);
-    hold on;
-end
-hold off;
-grid on;
-xlim([0 trace_offsets(end)-1]);
-set(gca, 'XTick', []);
-full_trace = cell2mat(traces);
-M = max(full_trace);
-m = min(full_trace);
-ylim([m M] + 0.1*(M-m)*[-1 1]);
-ylabel('Trace [a.u.]');
