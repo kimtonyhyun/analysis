acfd5f53394d8428b2b2db34fdb254fa7447b808
kimth <kimth@stanford.edu>
2026-04-20 06:05:27 -0700

    Revive

--- pca_ica/compute_ica_pairs.m
+++ pca_ica/compute_ica_pairs.m
@@ -1,17 +1,17 @@
 function [ica_filters, ica_traces] = compute_ica_pairs(pca_source, icaw_source)
 
 % Load PCA results
-load(pca_source, 'pca_info', 'pca_filters', 'pca_traces');
-height  = pca_info.movie_height;
-width   = pca_info.movie_width;
+pca = load(pca_source);
+height  = pca.info.movie_height;
+width   = pca.info.movie_width;
 
 % Load ICA weights
 load(icaw_source, 'ica_W');
 num_ICs = size(ica_W, 1);
 
 % Compute ICA pairs
-ica_traces  = ica_W*pca_traces;
-ica_filters = ica_W*pca_filters;
+ica_traces  = ica_W*pca.traces;
+ica_filters = ica_W*pca.filters;
 
 % Format the ICA dimensions for output
 ica_traces  = ica_traces'; % [time x IC]
--- pca_ica/run_ica.m
+++ pca_ica/run_ica.m
@@ -3,17 +3,17 @@ function run_ica(pca_source, num_ICs, mu)
 %   run_ica('_data/pca_n500.mat', 200, 0.1); % pca_source, num_ICs, mu
 
 % Load PCA results
-load(pca_source, 'pca_info', 'pca_filters', 'pca_traces');
+pca = load(pca_source);
 
 % ICA
 %------------------------------------------------------------
 fprintf('%s: Computing ICA weights...\n', datestr(now));
 
-ica_mixed = compute_spatiotemporal_ica_input(pca_filters, pca_traces, mu);
+ica_mixed = compute_spatiotemporal_ica_input(pca.filters, pca.traces, mu);
 
 term_tol = 1e-5; % Termination tolerance
 max_iter = 750;  % Max iterations of FastICA
-ica_W = compute_ica_weights(ica_mixed, num_ICs, term_tol, max_iter)'; %#ok<NASGU>
+ica_W = compute_ica_weights(ica_mixed, num_ICs, term_tol, max_iter)';
 
 info.type = 'ica';
 info.pca_source = pca_source;
@@ -23,13 +23,11 @@ info.mu = mu; %#ok<*STRNU>
 % Save ICA results
 %------------------------------------------------------------
 timestamp = datestr(now, 'yymmdd-HHMMSS');
-ica_savename  = sprintf('ica_%s.mat',  timestamp);
 icaw_savename = sprintf('icaw_%s.mat', timestamp);
-
 save(icaw_savename, 'info', 'ica_W'); % Save weights
 
 fprintf('%s: Computing ICA pairs (filters, traces) from weights...\n', datestr(now));
-[filters, traces] = compute_ica_pairs(pca_source, icaw_savename); %#ok<NASGU,ASGLU>
-save(ica_savename,  'info', 'filters', 'traces'); % Save ICA pairs
+[filters, traces] = compute_ica_pairs(pca_source, icaw_savename);
+save_rec(info, filters, traces);
 
 fprintf('%s: All done!\n', datestr(now));
